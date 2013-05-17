902ccfb32148b2c5e592b81edfeb7f81b33efac4
Bhakta Ranjan Satapathy <bhakta@motorolasolutions.com>
2013-05-17 21:30:40 +0530

    Added rhoconnect client application.

---
+++ manual/other/tnv_rhoconnectclient_code/public/api/generated/rhoapi-modules.js
@@ -0,0 +1,4 @@
+// WARNING! THIS FILE IS GENERATED AUTOMATICALLY! DO NOT EDIT IT MANUALLY!
+Rho.loadApiModules([
+    "Rho.RhoConnectClient","Rho.Application","Rho.Database","Rho.Database.SQLite3","Rho.Log","Rho.Network","Rho.System","Rho.System.Process","Rho.WebView"
+]);
