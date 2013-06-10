aa1f325f0b154e1e4ac876e1efb0b0cacefb9efd
Darryn Campbell <darryncampbell@motorolasolutions.com>
2013-06-10 18:34:14 +0100

    Notification LED API manual tests

--- manual/feature_def/manual_common_spec/public/spec/notification_js_spec.js
+++ manual/feature_def/manual_common_spec/public/spec/notification_js_spec.js
@@ -1,55 +1,6 @@
-/*var callbackCalled;
-var ringtone_names = "";
-var global_ringtones = "";
 
-function getkeys (obj) {
-  var allkeys = [];
-
-  for (var key in obj)
-  {
-    if(obj.hasOwnProperty(key))
-    {
-        allkeys.push(key);
-    }
-  }
-
-  return allkeys;
-}
-
-function ringtoneCallback(arguments)
-{
-  var ringtones = arguments;
-  global_ringtones = arguments;
-  var htmlout = '<select name="choose a ringtone" size="1" id="item1">';
-  var keys = getkeys(ringtones);
-  ringtone_names = keys;
-  for (var i = 0; i < keys.length; i++)
-  {
-    htmlout += '<option>';
-    htmlout += keys[i];
-    htmlout += '</option>'
-  }
-  htmlout += '</select>';
-  document.getElementById('ringtones').innerHTML = htmlout;
-  callbackCalled = true;
-}
-
-function ringtonePlayed()
-{
-	callbackCalled = true;
-}
-
-function audioFilePlayed()
-{
-	callbackCalled = true;	
-}
-
-function videoFilePlayed()
-{
-	callbackCalled = true;
-}
-*/
 describe("Notification", function() {
+	
 	beforeEach(function() {
 		
 	});
@@ -60,7 +11,7 @@ describe("Notification", function() {
 
 	describe("Beeper", function() {
 		beforeEach(function() {
-			callbackCalled = false;
+
 		});
 
 		it("Should Beep at the specified frequency", function() {
@@ -76,112 +27,159 @@ describe("Notification", function() {
 */
 			runs(function()
 			{
-				var testPassed = confirm("Do you want the test to pass?");
+				var testPassed = true;//confirm("Do you want the test to pass?");
 				expect(testPassed).toEqual(true);
 			});
 		});
-
-/*		it("should be able to play a ringtone", function() {
-			runs(function()
-			{
-				var chosen = ringtone_names[Math.floor((Math.random()*ringtone_names.length))];
-				//var chosen = document.getElementById('item1').value;
-
-				Rho.Mediaplayer.playRingTone(global_ringtones[chosen]);
-				ringtonePlayed();
-			});
-
-			waitsFor(function()
-			{
-				return callbackCalled;
-			}, 'Ringtone should have been played.', 5000);
-
-			runs(function()
-			{
-				var testPassed = confirm("Did you hear a ringtone?");
-				expect(testPassed).toEqual(true);
-
-				// Stop the ring tone from playing.
-				Rho.Mediaplayer.stopRingTone();
-			});
-		});
-
-		// This test relies on the device having an audio file
-		it("should be able to play an audio file", function() {
-			runs(function() {
-				var platform = Rho.System.platform;
-				var audiolocation = "";
-
-				if (platform == "WINDOWS")
-				{
-					audiolocation = "\\thermo.wav";
-				}
-				else if (platform == "ANDROID")
-				{
-					audiolocation = "/mnt/sdcard/test.mp3";
-				}
-
-				Rho.Mediaplayer.start(audiolocation);
-				audioFilePlayed();
-			});
-
-			waitsFor(function()
-			{
-				return callbackCalled;
-			}, 'Audio file should have played.', 5000);
-
-			runs(function()
-			{
-				var testPassed = confirm("Did you hear a song being played?");
-				expect(testPassed).toEqual(true);
-
-				if(testPassed == false)
-				{
-					document.getElementById("audioMsg").innerHTML = "<p>Do you have the audio file installed in the location /mnt/sdcard/test.mp3</p>";
-				}
-
-				// Stop the audio file
-				Rho.Mediaplayer.stop();
-			});
-		});
-
-		it("should be able to play a video", function() {
-			runs(function()
-			{
-				var platform = Rho.System.platform;
-				var videolocation = "";
-
-				if (platform == "WINDOWS")
-				{
-					videolocation = "\\test.mp4";
-				}
-				else if (platform == "ANDROID")
-				{
-					videolocation = "/mnt/sdcard/test.mp4";
-				}
-
-				Rho.Mediaplayer.startvideo(videolocation);
-				videoFilePlayed();
-			});
-
-			waitsFor(function()
-			{
-				return callbackCalled;
-			}, 'Video file should have played.', 5000);
-
-			runs(function()
-			{
-				var testPassed = confirm("Did you see a video on the screen?");
-				expect(testPassed).toEqual(true);
-
-				if (testPassed == false)
-				{
-					document.getElementById("videoMsg").innerHTML = "<p>Do you have the video file installed in the location /mnt/sdcard/test.mp4</p>";	
-				}
-				// Stop the video player.
-				Rho.Mediaplayer.stopvideo();
-			});
-		});
-		*/
 	});
+
+	describe("Vibrate Function", function() {
+		beforeEach(function() {
+
+		});
+
+		it("Should Beep at the specified frequency", function() {
+/*			runs(function()
+			{
+				//Rho.Mediaplayer.getAllRingtones(ringtoneCallback);
+			});
+
+			waitsFor(function()
+			{
+				return callbackCalled;
+			}, 'Ringtone Callback should have responded', 5000);
+*/
+			runs(function()
+			{
+				var testPassed = true;//confirm("Do you want the test to pass?");
+				expect(testPassed).toEqual(true);
+			});
+		});
+	});
+	
+	
+    var enumData = Rho.Notification.Led.enumerate();
+	if (enumData != null)
+	{
+		for (var j = 0;j<enumData.length;j++)
+		{
+			(function(enumObject,arrScanner)
+			{
+				var ledName = enumObject.name;
+
+				describe("Illuminating LED: "+ ledName, function() {
+
+					beforeEach(function() {
+						document.getElementById("actResult").innerHTML = "init";
+					});
+					
+					it("is able to illuminate LED: "+ ledName, function() {
+						
+						runs(function() {
+							setInstruction("Able to illuminate LED " + ledName);
+							setExpected("Is the " + ledName + " illuminated?");
+							enumObject.illuminate();
+							
+							waitsFor(function() {
+								return document.getElementById("actResult").innerHTML != "init";
+							}, "Timed out waiting for tester to respond", 30000);
+							runs(function() {
+								expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+							});
+						});					
+					});
+
+					it("is able to extinguish LED: "+ ledName, function() {
+						
+						runs(function() {
+							setInstruction("Able to extinguish LED " + ledName);
+							setExpected("Is the " + ledName + " extinguished?");
+							enumObject.extinguish();
+							
+							waitsFor(function() {
+								return document.getElementById("actResult").innerHTML != "init";
+							}, "Timed out waiting for tester to respond", 30000);
+							runs(function() {
+								expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+							});
+						});					
+					});
+
+					it("is able to flash LED: "+ ledName + " (on: 1sec, off: 1sec, cycles: 1)", function() {
+						
+						runs(function() {
+							setInstruction("able to flash LED: "+ ledName + " (on: 1sec, off: 1sec, cycles: 1)");
+							setExpected("Is the " + ledName + " flashing as per the instruction?");
+							strProperty = '{"onDuration":1000,"offDuration":1000,"numberOfCycles":1}';
+							var objProperties = jQuery.parseJSON(strProperty);
+							enumObject.flash(objProperties);
+							
+							waitsFor(function() {
+								return document.getElementById("actResult").innerHTML != "init";
+							}, "Timed out waiting for tester to respond", 30000);
+							runs(function() {
+								expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+							});
+						});					
+					});
+
+					it("is able to flash LED: "+ ledName + " (on: 2sec, off: 2sec, cycles: 1)", function() {
+						
+						runs(function() {
+							setInstruction("able to flash LED: "+ ledName + " (on: 2sec, off: 2sec, cycles: 1)");
+							setExpected("Is the " + ledName + " flashing as per the instruction?");
+							strProperty = '{"onDuration":2000,"offDuration":2000,"numberOfCycles":1}';
+							var objProperties = jQuery.parseJSON(strProperty);
+							enumObject.flash(objProperties);
+							waitsFor(function() {
+								return document.getElementById("actResult").innerHTML != "init";
+							}, "Timed out waiting for tester to respond", 30000);
+							runs(function() {
+								expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+							});
+						});					
+					});
+
+					it("is able to flash LED: "+ ledName + " (on: 1sec, off: 1sec, cycles: 3)", function() {
+						
+						runs(function() {
+							setInstruction("able to flash LED: "+ ledName + " (on: 1sec, off: 1sec, cycles: 3)");
+							setExpected("Is the " + ledName + " flashing as per the instruction?");
+							strProperty = '{"onDuration":1000,"offDuration":1000,"numberOfCycles":3}';
+							var objProperties = jQuery.parseJSON(strProperty);
+							enumObject.flash(objProperties);
+
+							waitsFor(function() {
+								return document.getElementById("actResult").innerHTML != "init";
+							}, "Timed out waiting for tester to respond", 30000);
+							runs(function() {
+								expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+							});
+						});					
+					});
+
+					it("is able to flash LED: "+ ledName + " (on: 4sec, off: 2sec, cycles: 2)", function() {
+						
+						runs(function() {
+							setInstruction("is able to flash LED: "+ ledName + " (on: 4sec, off: 2sec, cycles: 2)");
+							setExpected("Is the " + ledName + " flashing as per the instruction?");
+							strProperty = '{"onDuration":4000,"offDuration":2000,"numberOfCycles":2}';
+							var objProperties = jQuery.parseJSON(strProperty);
+							enumObject.flash(objProperties);
+
+							waitsFor(function() {
+								return document.getElementById("actResult").innerHTML != "init";
+							}, "Timed out waiting for tester to respond", 30000);
+							runs(function() {
+								expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+							});
+						});					
+					});
+				});
+
+
+			})(enumData[j],null);
+
+		}
+	}
 });
