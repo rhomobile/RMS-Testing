dddab2fb1bed395671a65a2d80a2fd45eac12d4c
Alexander Babichev <alexb@rhomobile.com>
2013-06-12 16:39:22 -0700

    Initial version rhoconnect push specs

---
+++ auto/feature_def/push_spec/rhoconnect_push_client/app/Settings/controller.rb
@@ -0,0 +1,102 @@
+require 'rho'
+require 'rho/rhocontroller'
+require 'rho/rhoerror'
+require 'helpers/browser_helper'
+require 'sync_server'
+require 'local_server'
+require 'push_server'
+
+
+class SettingsController < Rho::RhoController
+  include BrowserHelper
+
+  def index
+    @msg = @params['msg']
+
+    puts "Logged in: #{SyncEngine.logged_in}"
+=begin
+      if SyncEngine.logged_in
+          puts "logging out"
+          SyncEngine.logout
+          sleep 5
+      end
+=end
+	SyncEngine.set_syncserver("http://#{SYNC_SERVER_HOST}:#{SYNC_SERVER_PORT}")
+	RhoConf.set_property_by_name('rhoconnect_push_server',"http://#{PUSH_SERVER_HOST}:#{PUSH_SERVER_PORT}")
+	puts "push server is #{RhoConf.get_property_by_name('rhoconnect_push_server')}"
+
+      #    System.set_push_notification('/app/Settings/push_callback', '')
+	SyncEngine.login('pushclient', 'pushclient', '/app/Settings/login_callback' )
+  end
+
+  def login_callback
+    errCode = @params['error_code'].to_i
+
+    puts "login callback, error code: #{errCode}"
+
+    if errCode != 0
+      if errCode == Rho::RhoError::ERR_CUSTOMSYNCSERVER
+        @msg = @params['error_message']
+      end
+      if !@msg || @msg.length == 0
+        @msg = Rho::RhoError.new(errCode).message
+      end
+    end
+
+    host = SPEC_LOCAL_SERVER_HOST
+    port = SPEC_LOCAL_SERVER_PORT
+    Rho::AsyncHttp.get :url => "http://#{host}:#{port}?error=#{errCode}"
+
+    puts "sent error code to the spec server"
+    check_registration
+  end
+
+  def check_registration
+    puts "Check push registration"
+
+    host = SPEC_LOCAL_SERVER_HOST
+    port = SPEC_LOCAL_SERVER_PORT
+
+    if(Rho::RhoConfig.exist?('push_pin') && Rho::RhoConfig.push_pin != '')
+	  puts "RhoConfig: #{Rho::RhoConfig}"
+
+      puts "Sending device_id: #{Rho::RhoConfig.push_pin}"
+      Rho::AsyncHttp.get :url => "http://#{host}:#{port}?device_id=#{Rho::RhoConfig.push_pin}"
+    else
+      Rho::Timer.start(5000, url_for(:action=>'check_registration'), '')
+    end
+  end
+
+  def push_callback
+    puts "======> PUSH CALLBACK params #{@params.inspect}"
+
+    host = SPEC_LOCAL_SERVER_HOST
+    port = SPEC_LOCAL_SERVER_PORT
+    exit = nil
+
+    if @params['alert']
+      case @params['alert']
+      when 'exit'
+        exit = true
+        puts 'Exit command received!!!!!'
+      end
+    end
+
+    url = "http://#{host}:#{port}?alert=#{@params['alert']}&error=#{@params['error']}"
+    if @params['error']
+      puts "skipping response: #{url} <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+    else
+		  puts "sending response: #{url} <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+		  Rho::AsyncHttp.get :url => url
+    end
+
+    puts "before exit, exit = #{exit}"
+    if exit then
+      puts "before exit, exit = #{exit}"
+      System.exit
+      puts "after exit, exit = #{exit}"
+    end
+    puts "after exit, exit = #{exit}"
+  end
+
+end
---
+++ auto/feature_def/push_spec/rhoconnect_push_spec.rb
@@ -0,0 +1,222 @@
+require 'thread'
+require 'mspec'
+require 'restclient'
+require 'yaml'
+require 'psych' if RUBY_VERSION.to_s > "1.9.0"
+
+puts 'RUBY VERSION: ' + RUBY_VERSION.to_s
+TEST_PKGS = %w[com.rhomobile.rho_push_client com.motsolutions.cto.services.ans]
+
+cfgfilename = File.join(File.dirname(__FILE__),'config.yml')
+
+$rho_root = nil
+if File.file?(cfgfilename)
+	config = YAML::load_file(cfgfilename)
+	$rho_root = config["rhodes"]
+	$rhoconnect_root = config["rhoconnect"]
+	$rho_root = File.expand_path($rho_root) if $rho_root
+	$rhoconnect_root = File.expand_path($rhoconnect_root) if $rhoconnect_root
+	$rhoelements_root = config["rhoelements"]
+	$rhoelements_root = File.expand_path($rhoelements_root) if $rhoelements_root
+end
+
+unless $rho_root
+	$rho_root = `get-rhodes-info --rhodes-path`.chomp
+end
+
+puts "$rho_root is #{$rho_root}"
+
+$spec_path = FileUtils.pwd
+$platform = 'android'
+$device_id = nil
+$server = nil
+$requests = []
+$signal = ConditionVariable.new
+$mutex = Mutex.new
+# Assume redis up and running. Do not touch it!
+$rhoconnect_use_redis = false # true will start/stop it
+
+require File.join($rho_root,'lib','build','jake.rb')
+require File.join($rho_root,'platform','android','build','android_tools.rb')
+require_relative './rhoconnect_helper.rb'
+require './run_rhoconnect_spec'
+
+describe 'Rhoconnect push spec' do
+  before(:all) do
+	  #TODO: check that Rhoelements gem is installed
+    puts "Starting local server"
+    $server, addr, port = Jake.run_local_server
+    File.open(File.join($spec_path, 'rhoconnect_push_client', 'app', 'local_server.rb'), 'w') do |f|
+      f.puts "SPEC_LOCAL_SERVER_HOST = '#{addr}'"
+      f.puts "SPEC_LOCAL_SERVER_PORT = #{port}"
+    end
+
+    $server.mount_proc('/', nil) do |req, res|
+      query = req.query
+      puts "Request headers: #{req.header.inspect}"
+      puts "Request query: #{query.inspect}"
+
+      res.status = 200
+      $mutex.synchronize do
+        $requests << req
+        $signal.signal
+      end
+    end
+
+    run_apps($platform)
+
+    @api_token = RhoconnectHelper.api_post('system/login', { :login => 'rhoadmin', :password => '' })
+    puts "API token: #{@api_token}"
+  end
+
+
+  after(:all) do
+    stop_apps
+    cleanup_apps
+    # FIXME:
+    # `adb emu kill`
+
+    # FIXME:
+    TEST_PKGS.each do |pkg|
+      puts "Uninstalling package #{pkg} ..."
+      system "adb uninstall #{pkg}"
+    end
+    # puts "Uninstalling package com.rhomobile.rho_push_client ..."
+    # system "adb uninstall com.rhomobile.rho_push_client"
+    # puts "Uninstalling package com.motsolutions.cto.services.ans ..."
+    # system "adb uninstall com.motsolutions.cto.services.ans"
+  end
+
+  def expect_request(name)
+    val = nil
+    $mutex.synchronize do
+		  $signal.wait($mutex, 60) # wait timeout 60 secs.
+		  $requests.count.should == 1
+		  val = $requests.first.query[name]
+		  $requests.clear
+    end
+    val
+  end
+
+  it 'should login' do
+    puts 'Waiting message with login errCode'
+    expect_request('error').should == "0"
+  end
+
+  it 'should register' do
+    puts 'Waiting message with Rhoconnect registaration...'
+    $device_id = expect_request('device_id')
+    $device_id.should_not be_nil
+    $device_id.should_not == ''
+  end
+
+  it 'should proceed push message at foreground' do
+    sleep 5
+    puts 'Sending push message...'
+
+    message = 'magic1'
+    params = { :user_id=>['pushclient'], :message=>message }
+    RhoconnectHelper.api_post('users/ping',params,@api_token)
+
+    puts 'Waiting message with push content...'
+    expect_request('alert').should == message
+
+    sleep 3
+  end
+
+  # it 'should proceed push message with exit comand' do
+  #   puts 'Sending push message with exit command...'
+
+  #   message = 'exit'
+  #   params = { :user_id=>['pushclient'], :message=>message }
+  #   RhoconnectHelper.api_post('users/ping',params,@api_token)
+
+  #   puts 'Waiting message with push content...'
+  #   expect_request('alert').should == message
+
+  #   sleep 5
+
+  #   output = Jake.run2('adb', ['-e', 'shell', 'ps'], {:hide_output=>true})
+  #   (output =~ /rho_push_client/).should be_nil
+  # end
+
+  # it 'should process push message' do
+  #   puts 'Sending push message with greeting ...'
+  #   message = 'Hello'
+  #   params = { :user_id=>['pushclient'], :message => message}
+  #   RhoconnectHelper.api_post('users/ping',params,@api_token)
+
+  #   puts 'Waiting ping message with push content ...'
+  #   expect_request('alert').should == message
+
+  #   sleep 5
+
+  #   output = Jake.run2('adb', ['-e', 'shell', 'ps'], {:hide_output => true})
+  #  (output =~ /rho_push_client/).should_not be_nil
+  # end
+
+  # it 'should process sequence of push messages' do
+  #   puts 'Sending 5 push messages...'
+
+  #   alerts = {}
+  #   5.times do |i|
+  #     message = "magic#{i}"
+  #     alerts[message] = true
+  #     params = { :user_id=>['pushclient'], :message => message}
+  #     RhoconnectHelper.api_post('users/ping',params,@api_token)
+  #     sleep 0.5
+  #   end
+
+  #   puts 'Waiting 5 messages with push content...'
+
+  #   5.times do |i|
+  #     $mutex.synchronize do
+  #       break if $requests.count == 5
+  #       $signal.wait($mutex, 60)
+  #     end
+  #     puts "Message count: #{$requests.count}"
+  #   end
+
+  #   $requests.count.should == 5
+
+  #   $mutex.synchronize do
+  #     puts alerts.inspect
+  #     5.times do |i|
+  #       message = $requests[i].query['alert']
+  #       message.should_not be_nil
+  #       puts "message: #{message}"
+  #       alerts[message].should be_true
+  #       alerts[message] = false
+  #       #$requests.delete_at 0
+  #     end
+  #     $requests.clear
+  #   end
+  # end
+
+
+  # TODO:
+  # it 'should start processing sequence of push messaxges' do
+  #   puts 'Sending push message with exit command...'
+
+  #   message = 'exit'
+  #   params = { :user_id=>['pushclient'], :message=>message }
+  #   RhoconnectHelper.api_post('users/ping', params, @api_token)
+
+  #   puts 'Waiting message with push content...'
+  #   $mutex.synchronize do
+  #     $signal.wait($mutex, 60)
+  #     $requests.count.should == 1
+  #     alert = $requests.first.query['alert']
+  #     alert.should_not be_nil
+  #     alert.should == message
+  #     $requests.clear
+  #   end
+
+  #   sleep 5
+  #   output = Jake.run2('adb', ['-e', 'shell', 'ps'], {:hide_output=>true})
+  #   puts output
+
+  #   (output =~ /rho_push_client/).should be_nil
+  # end
+
+end
