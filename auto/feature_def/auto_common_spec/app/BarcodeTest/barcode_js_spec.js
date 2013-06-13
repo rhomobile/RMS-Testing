68ecb484b216dbf1e92e9affd7fe46a287175d18
Patro <cqvf76@motorolasolutions.com>
2013-06-13 18:18:31 +0530

    after commenting the Barcode enable spec in barcode_spc_js in auto

--- auto/feature_def/auto_common_spec/app/BarcodeTest/barcode_js_spec.js
+++ auto/feature_def/auto_common_spec/app/BarcodeTest/barcode_js_spec.js
@@ -53,6 +53,7 @@ describe("Barcode JS API Test", function() {
 				}
 			});
 
+
 			describe("Barcode property Using set/getProperties for "+ scntype +": "+ scnname, function() {
 
 				for (var i=0;i<arrScanner.length;i++){
@@ -130,6 +131,70 @@ describe("Barcode JS API Test", function() {
 				}
 			});
 
+			describe("getProperty and get properties with all combination for "+ scntype +": "+ scnname, function() {
+
+						var getpropertiesdata1 ='';
+						var getpropertiesdata ='';
+						var getpropertydata ='';
+						var getpropertydata1 ='';
+
+						var callbackgetproperties = function (data){
+							getpropertiesdata = JSON.stringify(data);
+						}
+
+						var callbackgetproperty = function (data){
+							getpropertydata = data;
+						}
+
+						it("VT282-2001 | call getProperties() with callback as action URL and hash |" + scntype, function() {
+
+							    enumObject.clearAllProperties();
+							    enumObject.setProperties({'allDecoders':false,'picklistMode':'softwareReticle','code93':true});
+								enumObject.getProperties(['allDecoders','picklistMode','code93'],callbackgetproperties);
+								expect(getpropertiesdata).toContain('true');
+								expect(getpropertiesdata).toContain('false');
+								expect(getpropertiesdata).toContain('softwareReticle');								
+						});
+
+
+						it("VT282-2002 | call getProperties() with anonymous callback and hash |" + scntype, function() {
+
+							    enumObject.clearAllProperties();							    
+							    enumObject.setProperties({'allDecoders':false,'picklistMode':'softwareReticle','code93':true});
+								enumObject.getProperties(['allDecoders','picklistMode','code93'],function(data){getpropertiesdata1 = JSON.stringify(data);});
+								expect(getpropertiesdata1).toContain('true');
+								expect(getpropertiesdata1).toContain('false');
+								expect(getpropertiesdata1).toContain('softwareReticle');								
+						});
+
+
+						it("VT282-2004 | call getProperty() with sync callback and property |" + scntype, function() {
+
+							    enumObject.clearAllProperties();							    
+							    enumObject.setProperty(allDecoders,true);
+								enumObject.getProperty(allDecoders,callbackgetproperty);
+								expect(getpropertydata).toEqual('true');								
+						});
+
+						it("VT282-2005 | call getProperty() with anonymous callback and property |" + scntype, function() {
+
+							    enumObject.clearAllProperties();							    
+							    enumObject.setProperty(picklistMode,softwareReticle);
+								enumObject.getProperty(picklistMode,function(data){getpropertydata1 = data;});
+								expect(getpropertydata1).toEqual('true');							
+						});
+
+
+						it("VT282-2006 | call getDefault and enable |" + scntype, function() {
+
+							    enumObject.clearAllProperties();
+							    Rho.Barcode.setDefault(enumObject);
+							    var defaultobj = Rho.Barcode.getDefault(enumObject);							  
+								expect(scntype).toEqual(defaultobj.getProperty('ID'));	
+						});
+
+			});
+
 			describe("Disable Scanner "+ scntype +": "+ scnname, function() {
 
 				beforeEach(function() {
@@ -154,6 +219,7 @@ describe("Barcode JS API Test", function() {
 				});
 			});
 
+/*
 			describe("Barcode property set using enable() for "+ scntype +": "+ scnname, function() {
 
 				var flag = false;
@@ -218,6 +284,7 @@ describe("Barcode JS API Test", function() {
 
 				}
 			});
+*/
 
 		})(enumData[j],arrSCN);
 
@@ -234,7 +301,7 @@ describe("Enumerate Scanner ", function() {
 			Rho.Barcode.enumerate(enumCallback);
 			expect(enumCallback).toEqual(true);
 		});
-	 });
+	});
 
 	it("Enumerate Scanner with anonymous function as callback", function() {
 			
