3692a45d187b6ccf26266e0c7d02d6940c5103d1
Patro <cqvf76@motorolasolutions.com>
2013-06-18 21:07:56 +0530

    Added more test for Barcode_sw_js_spec and sepcrunnerSW

--- manual/feature_def/manual_common_spec/app/Barcode/barcode_sw_js_spec.js
+++ manual/feature_def/manual_common_spec/app/Barcode/barcode_sw_js_spec.js
@@ -3,18 +3,9 @@ describe("Barcode Test", function() {
 	var enableFlag = false;
 	var decodeFlag = false;
 	//var enumData = '';
-	var decodedata ='';
-	var callbackfired = false;
-
-	var callbackenable = function (data){
-			decodedata = data;
-			displayResult("Data:- ",JSON.stringify(decodedata));
-			callbackfired = true;			
-		}
 
 	var callbacktake = function (data){
-			takedata = data;
-			displayResult("take Data:- ",JSON.stringify(takedata));
+			enablecallbackdata(JSON.stringify(data));
 		}
 
 	enumData = Rho.Barcode.enumerate();
@@ -29,8 +20,8 @@ describe("Barcode Test", function() {
 		enableFlag = false;
 		decodeFlag = false;
 		decodedata ='';
-		callbackfired = false;
-		/* ... Set up your object ... */
+		document.getElementById("actResult").innerHTML = "init";
+		enablecallbackdata(decodedata);
 	});
 
 	afterEach(function() {
@@ -43,101 +34,363 @@ describe("Barcode Test", function() {
 
 		runs(function()
 		{
-			Rho.Barcode.setDefaultID( objSCN.getId() );
+			setObjective("VT282-1778 | call setDefault and take");
+			setInstruction("wait for viewfinder to come automatically for " + scnid + " Scan any barcode");
+			setExpected("code128 barcode should decode and retrun value should be decoded data and status");
+			Rho.Barcode.setDefaultID(objSCN.getId());
 			Rho.Barcode.take({},callbacktake);
 			setTimeout(function() {
 				enableFlag = true;
-			}, 8000);
-		});
-
-		runs(function()
-		{		
-			setTimeout(function() {
-				decodeFlag = true;
-			}, 15000);
+			}, 4000);
 		});
 
 		waitsFor(function()
 		{
-			dispCurrentProcess("scan any barcode <br/> check for retrurn value with take");
-			return decodeFlag;
-		}, '15sec Wait to Scan the Barcode', 16000);
+			return enableFlag;
+		}, '5sec wait to enable the Scanner', 5000);
 
 		runs(function()
-		{
-			var testPassed = confirm("Do you see Correct Behavior?");
-			expect(testPassed).toEqual(true);
+		{		
+			waitsFor(function() {
+			return document.getElementById("actResult").innerHTML != "init";
+			}, "Timed out waiting for tester to respond", 300000);
+			runs(function() {
+			expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+			objSCN.disable();
+			});	
 		});
 
 	});
-
-
-
 
 	it("VT282-1790 | take with callback as function |"+ scnid, function() {
 		
 		runs(function()
 		{
+			setObjective("VT282-1790 | take with callback as function|");
+			setInstruction("wait for scanner to start and scan any barcode after strating scanner" + scnid);
+			setExpected("scanner should start comeup automatically and Decoded data and status only should be returned ");
 			objSCN.take({},callbacktake);
 			setTimeout(function() {
 				enableFlag = true;
-			}, 8000);
-		});
-
-		runs(function()
-		{		
-			setTimeout(function() {
-				decodeFlag = true;
-			}, 15000);
+			}, 4000);
 		});
 
 		waitsFor(function()
 		{
-			dispCurrentProcess("scan any barcode <br/> check for retrurn value with take");
-			return decodeFlag;
-		}, '15sec Wait to Scan the Barcode', 16000);
+			return enableFlag;
+		}, '5sec wait to enable the Scanner', 5000);
 
 		runs(function()
-		{
-			var testPassed = confirm("Do you see Correct Behavior?");
-			expect(testPassed).toEqual(true);
+		{		
+			waitsFor(function() {
+			return document.getElementById("actResult").innerHTML != "init";
+			}, "Timed out waiting for tester to respond", 300000);
+			runs(function() {
+			expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+			//objSCN.disable();
+			});
 		});
 	});
-
-
 
 
 	it("VT282-1797 | Take with callback as anonymous function |"+ scnid, function() {
 		
 		runs(function()
 		{
-			objSCN.take({},function(data){displayResult("take Data:- ",JSON.stringify(data));});
+			setObjective("VT282-1797 | Take with callback as anonymous function|");
+			setInstruction("scan any barcode with" + scnid);
+			setExpected("data of barcode and status as Ok should be returned");
+			objSCN.take({},function(data){enablecallbackdata(JSON.stringify(data));});
 			setTimeout(function() {
 				enableFlag = true;
-			}, 8000);
-		});
-
-		runs(function()
-		{		
-			setTimeout(function() {
-				decodeFlag = true;
-			}, 15000);
+			}, 4000);
 		});
 
 		waitsFor(function()
 		{
-			dispCurrentProcess("Scan any Barcode <br/> Check for the retruned array of take");
-			return decodeFlag;
-		}, '15sec Wait to Scan the Barcode', 16000);
+			return enableFlag;
+		}, '5sec wait to enable the Scanner', 5000);
 
 		runs(function()
-		{
-			var testPassed = confirm("Do you see Correct Behavior?");
-			expect(testPassed).toEqual(true);
+		{		
+			waitsFor(function() {
+			return document.getElementById("actResult").innerHTML != "init";
+			}, "Timed out waiting for tester to respond", 300000);
+			runs(function() {
+			expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+			objSCN.disable();
+			});
 		});
 
 	});
 
+
+	it("VT282-1790A | take with callback as function and don't scan|"+ scnid, function() {
+		
+		runs(function()
+		{
+			setObjective("VT282-1790A | take with callback as function and don't scan|");
+			setInstruction("Don't scan any barcode after scanner starts automatically and wait for scanner to stop" + scnid);
+			setExpected("Decoded data should come as nil and status should be cancel and Beam ore viewfinder will go after 10 sec ");
+			objSCN.take({},callbacktake);
+			setTimeout(function() {
+				enableFlag = true;
+			}, 4000);
+		});
+
+		waitsFor(function()
+		{
+			return enableFlag;
+		}, '5sec wait to enable the Scanner', 5000);
+
+		runs(function()
+		{		
+			waitsFor(function() {
+			return document.getElementById("actResult").innerHTML != "init";
+			}, "Timed out waiting for tester to respond", 300000);
+			runs(function() {
+			expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+			//objSCN.disable();
+			});
+		});
+	});
+
+
+	it("VT282-1994 | call getAllProperties() with async callback |"+ scnid, function() {
+
+		runs(function()
+		{
+			setObjective("VT282-1994 | call getAllProperties() with async callback |");
+			setInstruction("Don't scan and check for all the supported propertylist for scanner" + scnid);
+			setExpected("all the supported properties with their default value should return in callback");
+			objSCN.getAllProperties(callbacktake);
+			setTimeout(function() {
+				enableFlag = true;
+			}, 4000);
+		});
+		waitsFor(function()
+		{
+			return enableFlag;
+		}, '5sec wait to enable the Scanner', 5000);
+
+		runs(function()
+		{		
+			waitsFor(function() {
+			return document.getElementById("actResult").innerHTML != "init";
+			}, "Timed out waiting for tester to respond", 300000);
+			runs(function() {
+			expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+			//objSCN.disable();
+			});
+		});
+	});
+
+	it("VT282-1998 | call getAllProperties() without callback(Sync Access) |"+ scnid, function() {
+		
+		runs(function()
+		{
+			setObjective("VT282-1998 | call getAllProperties() without callback(Sync Access) |");
+			setInstruction("Don't scan and check for all the supported propertylist for scanner" + scnid);
+			setExpected("all the supported properties with their default value should return with Sync Access");
+			var data = objSCN.getAllProperties();
+			callbacktake(data);
+			setTimeout(function() {
+				enableFlag = true;
+			}, 4000);
+		});
+		waitsFor(function()
+		{
+			return enableFlag;
+		}, '5sec wait to enable the Scanner', 5000);
+
+		runs(function()
+		{		
+			waitsFor(function() {
+			return document.getElementById("actResult").innerHTML != "init";
+			}, "Timed out waiting for tester to respond", 300000);
+			runs(function() {
+			expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+			//objSCN.disable();
+			});
+		});
+	});
+
+	it("VT282-1999 | call getAllProperties() with anonymous callback |"+ scnid, function() {
+
+		runs(function()
+		{
+			setObjective("VT282-1999 | call getAllProperties() with anonymous callback |");
+			setInstruction("Don't scan and check for all the supported propertylist for scanner" + scnid);
+			setExpected("all the properties with their default value should return with anonymous callback");
+			objSCN.getAllProperties(function(data){enablecallbackdata(JSON.stringify(data));});
+			setTimeout(function() {
+				enableFlag = true;
+			}, 4000);
+		});
+		waitsFor(function()
+		{
+			return enableFlag;
+		}, '5sec wait to enable the Scanner', 5000);
+
+		runs(function()
+		{		
+			waitsFor(function() {
+			return document.getElementById("actResult").innerHTML != "init";
+			}, "Timed out waiting for tester to respond", 300000);
+			runs(function() {
+			expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+			//objSCN.disable();
+			});
+		});
+	});
+
+
+	it("VT282-2008 | call getSupportedProperties() with anonymous callback |"+ scnid, function() {
+		
+		runs(function()
+		{
+			setObjective("VT282-2008 | call getSupportedProperties() with anonymous callback |");
+			setInstruction("Don't scan and check for the supported propertylist for scanner" + scnid);
+			setExpected("all the supported properties should return with anonymous callback");
+			objSCN.getSupportedProperties(function(data){enablecallbackdata(JSON.stringify(data));});
+			setTimeout(function() {
+				enableFlag = true;
+			}, 4000);
+		});
+		waitsFor(function()
+		{
+			return enableFlag;
+		}, '5sec wait to enable the Scanner', 5000);
+
+		runs(function()
+		{		
+			waitsFor(function() {
+			return document.getElementById("actResult").innerHTML != "init";
+			}, "Timed out waiting for tester to respond", 300000);
+			runs(function() {
+			expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+			//objSCN.disable();
+			});
+		});
+	});
+
+	it("VT282-2008A | call getSupportedProperties() with async callback |"+ scnid, function() {
+
+		runs(function()
+		{
+			setObjective("VT282-2008A | call getSupportedProperties() with async callback |");
+			setInstruction("check for all the supported propertylist for scanner" + scnid);
+			setExpected("all the supported properties should return in async callback");
+			objSCN.getSupportedProperties(callbacktake);
+			setTimeout(function() {
+				enableFlag = true;
+			}, 4000);
+		});
+		waitsFor(function()
+		{
+			return enableFlag;
+		}, '5sec wait to enable the Scanner', 5000);
+
+		runs(function()
+		{		
+			waitsFor(function() {
+			return document.getElementById("actResult").innerHTML != "init";
+			}, "Timed out waiting for tester to respond", 300000);
+			runs(function() {
+			expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+			//objSCN.disable();
+			});
+		});
+	});
+
+
+	it("VT282-2008B | call getProperties() with ID without callback |"+ scnid, function() {
+
+		runs(function()
+		{
+			setObjective("VT282-2008A | call getProperties() with ID without callback|");
+			setInstruction("check for retrurned value" + scnid);
+			setExpected("It should return the Scanner ID");
+			var data = objSCN.getProperties(['ID']);
+			callbacktake(data);
+			setTimeout(function() {
+				enableFlag = true;
+			}, 4000);
+		});
+		waitsFor(function()
+		{
+			return enableFlag;
+		}, '5sec wait to enable the Scanner', 5000);
+
+		runs(function()
+		{		
+			waitsFor(function() {
+			return document.getElementById("actResult").innerHTML != "init";
+			}, "Timed out waiting for tester to respond", 300000);
+			runs(function() {
+			expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+			//objSCN.disable();
+			});
+		});
+	});
+
+	it("VT282-2008C | call getProperties() with ID with callback |"+ scnid, function() {
+
+		runs(function()
+		{
+			setObjective("VT282-2008A | call getProperties() with ID with callback|");
+			setInstruction("check for retrurned value" + scnid);
+			setExpected("It should return the Scanner ID");
+			objSCN.getProperties(['ID'],callbacktake);
+			setTimeout(function() {
+				enableFlag = true;
+			}, 4000);
+		});
+		waitsFor(function()
+		{
+			return enableFlag;
+		}, '5sec wait to enable the Scanner', 5000);
+
+		runs(function()
+		{		
+			waitsFor(function() {
+			return document.getElementById("actResult").innerHTML != "init";
+			}, "Timed out waiting for tester to respond", 300000);
+			runs(function() {
+			expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+			//objSCN.disable();
+			});
+		});
+	});
+
+	it("VT282-2008D | call getProperty() with ID with anonymous callback |"+ scnid, function() {
+
+		runs(function()
+		{
+			setObjective("VT282-2008A | call getProperty() with ID with anonymous callback|");
+			setInstruction("check for retrurned value" + scnid);
+			setExpected("It should return the Scanner ID");
+			objSCN.getProperty(['ID'],function(data){enablecallbackdata(JSON.stringify(data));});
+			setTimeout(function() {
+				enableFlag = true;
+			}, 4000);
+		});
+		waitsFor(function()
+		{
+			return enableFlag;
+		}, '5sec wait to enable the Scanner', 5000);
+
+		runs(function()
+		{		
+			waitsFor(function() {
+			return document.getElementById("actResult").innerHTML != "init";
+			}, "Timed out waiting for tester to respond", 300000);
+			runs(function() {
+			expect("pass").toEqual(document.getElementById("actResult").innerHTML);
+			//objSCN.disable();
+			});
+		});
+	});
 
 })(enumData[j]);
 
