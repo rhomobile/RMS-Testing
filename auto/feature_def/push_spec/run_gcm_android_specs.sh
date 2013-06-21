3178cd483ccf10bc2a005d536245b097a28810d9
Alexander Babichev <alexb@rhomobile.com>
2013-06-21 14:03:03 -0700

    Update run scripts and set android version to latest release

--- auto/feature_def/push_spec/run_gcm_android_specs.sh
+++ auto/feature_def/push_spec/run_gcm_android_specs.sh
@@ -2,3 +2,4 @@
 # Google Cloud Messaging specs for android device
 
 mspec gcm_push_spec.rb
+if (($?)) ; then echo "Google Cloud Messaging specs are failed "; exit 1; fi
--- auto/feature_def/push_spec/run_rps_wm_specs.sh
+++ auto/feature_def/push_spec/run_rps_wm_specs.sh
@@ -3,3 +3,4 @@
 
 mspec win_push_spec.rb
 
+if (($?)) ; then echo "Rhoconnect push specs for windows mobile/CE failed "; exit 1; fi
