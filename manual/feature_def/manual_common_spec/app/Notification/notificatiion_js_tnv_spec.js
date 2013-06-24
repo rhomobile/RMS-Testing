36c53b686e2508613a89f69ab9413ac3d8408bf3
Ben Kennedy <ben.kennedy@motorolasolutions.com>
2013-06-24 14:50:12 +0100

    Android: Notification: Fixed timings in tests and did Android specific tests (vibrate duration)

--- manual/feature_def/manual_common_spec/app/Notification/notificatiion_js_tnv_spec.js
+++ manual/feature_def/manual_common_spec/app/Notification/notificatiion_js_tnv_spec.js
@@ -160,7 +160,7 @@ describe("Notification Manual FD  Tests", function () {
                 {id: 'yes', title: 'yes'},
                 'No',
                 'Cancel'
-            ], title: 'MyTitle', icon: '/app/Notification/icon.png'};
+            ], title: 'MyTitle', icon: Rho.RhoFile.join(Rho.Application.modelFolderPath('Notification'), 'icon.png')};
             Rho.Notification.showPopup(propertyMap);
         });
 
@@ -230,7 +230,7 @@ if ( Rho.System.platform == "WINDOWS_DESKTOP" || Rho.System.platform == "WINDOWS
             Rho.Notification.showStatus('MyAlert', 'This is status message', 'click to hide');
             setTimeout(function () {
                 Rho.Notification.hidePopup();
-            }, 1000);
+            }, 10000);
         });
         waitsFor(function () {
             dispExpectedResult("see if the pop up is showing the message, then it hides automatically after 10sec");
@@ -260,7 +260,7 @@ if ( Rho.System.platform == "WINDOWS_DESKTOP" || Rho.System.platform == "WINDOWS
             
             setTimeout(function () {
                 Rho.Notification.hidePopup();
-            }, 1000);
+            }, 10000);
         });
         waitsFor(function () {
             dispExpectedResult("see if the pop up is showing the message, then it hides automatically after 10sec");
@@ -314,17 +314,17 @@ if ( Rho.System.platform == "WINDOWS_DESKTOP" || Rho.System.isRhoSimulator )
             expect(testResult).toEqual(true);
         });
     });
-    it("VT281-0858 |Beep for 10 secs and with volume one  with 2000 hz|", function () {
+    it("VT281-0858 |Beep for 2 secs and with volume one  with 2000 hz|", function () {
 
         runs(function () {
             dispTestCaseRunning(" Beeper will be started if its applicable for the Device  ");
-            dispExpectedResult(" Beeper should sound for 10 secs and with volume 1 and frequency 10000, please observe the change in the volume from previous case  ");
+            dispExpectedResult(" Beeper should sound for 2 secs and with volume 1 and frequency 10000, please observe the change in the volume from previous case  ");
             var propertyMap = {frequency: 10000, volume: 1, duration: 2000};
             Rho.Notification.beep(propertyMap);
         });
 
         waitsFor(function () {
-            dispExpectedResult("Beeper should sound for 10 secs and with volume 1 and frequency 2000  ");
+            dispExpectedResult("Beeper should sound for 2 secs and with volume 1 and frequency 2000  ");
             return captured;
         }, 'Beep sound should have ended by now', 30000);
 
@@ -458,40 +458,78 @@ if ( Rho.System.platform == "WINDOWS_DESKTOP" || Rho.System.isRhoSimulator )
             expect(testResult).toEqual(true);
         });
     });
-
-    it("VT281-0861|Vibrate with duration 25 sec|", function () {
-
-        runs(function () {
-            dispTestCaseRunning(" Device will vibrate if applicable ");
-            dispExpectedResult("Device should vibrate for 25 seconds ");
-            Rho.Notification.vibrate(25000);
-        });
-
-        waitsFor(function () {
-            dispExpectedResult("Device should vibrate for 25 seconds");
-            return captured;
-        }, 'Tester should ve responded by now ', 30000);
-
-        runs(function () {
-            expect(testResult).toEqual(true);
-        });
-    });
-
-    it("VT281-0861|Vibrate with duration 27 sec|", function () {
-        runs(function () {
-            dispTestCaseRunning(" Device will vibrate if applicable ");
-            dispExpectedResult("Device should vibrate for ONLY 25 seconds even though it is 27 sec as 25 is the max value it can take");
-            Rho.Notification.vibrate(27000);
-        });
-        waitsFor(function () {
-            dispExpectedResult("Device should vibrate for ONLY 25 seconds even though it is 27 sec as 25 is the max value it can take")
-            return captured;
-        }, 'Tester should ve responded by now ', 30000);
-
-        runs(function () {
-            expect(testResult).toEqual(true);
-        });
-    });
+	if(Rho.System.platform == "ANDROID")
+	{
+	    it("VT281-0861|Vibrate with duration 15 sec|", function () {
+	
+	        runs(function () {
+	            dispTestCaseRunning(" Device will vibrate if applicable ");
+	            dispExpectedResult("Device should vibrate for 15 seconds ");
+	            Rho.Notification.vibrate(15000);
+	        });
+	
+	        waitsFor(function () {
+	            dispExpectedResult("Device should vibrate for 15 seconds");
+	            return captured;
+	        }, 'Tester should ve responded by now ', 30000);
+	
+	        runs(function () {
+	            expect(testResult).toEqual(true);
+	        });
+	    });
+	
+	    it("VT281-0861|Vibrate with duration 17 sec|", function () {
+	        runs(function () {
+	            dispTestCaseRunning(" Device will vibrate if applicable ");
+	            dispExpectedResult("Device should vibrate for ONLY 15 seconds even though it is 17 sec as 15 is the max value it can take");
+	            Rho.Notification.vibrate(17000);
+	        });
+	        waitsFor(function () {
+	            dispExpectedResult("Device should vibrate for ONLY 15 seconds even though it is 17 sec as 15 is the max value it can take")
+	            return captured;
+	        }, 'Tester should ve responded by now ', 30000);
+	
+	        runs(function () {
+	            expect(testResult).toEqual(true);
+	        });
+	    });
+	}
+	else
+	{
+	    it("VT281-0861|Vibrate with duration 25 sec|", function () {
+	
+	        runs(function () {
+	            dispTestCaseRunning(" Device will vibrate if applicable ");
+	            dispExpectedResult("Device should vibrate for 25 seconds ");
+	            Rho.Notification.vibrate(25000);
+	        });
+	
+	        waitsFor(function () {
+	            dispExpectedResult("Device should vibrate for 25 seconds");
+	            return captured;
+	        }, 'Tester should ve responded by now ', 30000);
+	
+	        runs(function () {
+	            expect(testResult).toEqual(true);
+	        });
+	    });
+	
+	    it("VT281-0861|Vibrate with duration 27 sec|", function () {
+	        runs(function () {
+	            dispTestCaseRunning(" Device will vibrate if applicable ");
+	            dispExpectedResult("Device should vibrate for ONLY 25 seconds even though it is 27 sec as 25 is the max value it can take");
+	            Rho.Notification.vibrate(27000);
+	        });
+	        waitsFor(function () {
+	            dispExpectedResult("Device should vibrate for ONLY 25 seconds even though it is 27 sec as 25 is the max value it can take")
+	            return captured;
+	        }, 'Tester should ve responded by now ', 30000);
+	
+	        runs(function () {
+	            expect(testResult).toEqual(true);
+	        });
+	    });
+	}
 }    
 });
 
