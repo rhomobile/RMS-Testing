e39f47868a0ed08578048296b7e11b7738caab3b
Alexander Babichev <alexb@rhomobile.com>
2013-06-25 16:39:57 -0700

    Implement multiple device support for adnroid specs

--- auto/feature_def/push_spec/run_rps_android_specs.sh
+++ auto/feature_def/push_spec/run_rps_android_specs.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 # Rhoconnect push specs for android
 
+# path to local rhodes workspace
+pushd ../../../../rhodes
+git pull origin master
+popd
+
+# path to local rhoconnect workspace
+pushd ../../../../rhoconnect
+git pull origin master
+popd
+
+# path to rhoelements
+pushd ../../../../Motorola-Extensions
+git pull origin master
+popd
+
 pushd rhoconnect_push_client
-# rake clean:android
+echo -e "\nClean android rhodes app ..."
+rake clean:android
+# echo -e "\nBuilding rhodes app ..."
+# rake device:android:debug
+# if (($?)) ; then echo "Cannot build rhodes app"; exit 1; fi
 popd
 
 mspec android_push_spec.rb
-
-if (($?)) ; then echo "Rhoconnect push specs for android are failed "; exit 1; fi
+if (($?)) ; then echo "Rhoconnect push specs for android are failed"; exit 1; fi
--- auto/feature_def/push_spec/spec_helper.rb
+++ auto/feature_def/push_spec/spec_helper.rb
@@ -1,9 +1,6 @@
 require 'fileutils'
 require 'rake'
 
-load File.join($rho_root,'Rakefile')
-load File.join($rho_root,'platform','android','build','android.rake')
-
 def stop_apps
 	RhoconnectHelper.stop_rhoconnect_stack
 end
@@ -16,7 +13,10 @@ def cleanup_apps
 	end
 end
 
-def run_apps(platform)
+def run_apps(platform, build_required)
+	load File.join($rho_root,'Rakefile')
+	load File.join($rho_root,'platform','android','build','android.rake')
+
 	appname = "rhoconnect_push_client"
 	test_appname = "testapp"
 
@@ -37,13 +37,14 @@ def run_apps(platform)
 	FileUtils.mkdir_p File.expand_path($tmp_path)
 	$server_path = File.expand_path(File.join($tmp_path,'testapp'))
 
-
 	RhoconnectHelper.set_rhoconnect_bin "#{$rhoconnect_root}/bin/rhoconnect"
 	puts "rhoconnect_bin: #{RhoconnectHelper.rhoconnect_bin}"
 
 	RhoconnectHelper.set_rc_push_out File.open( File.join($app_path, "rhoconnect_push.log" ), "w")
 	RhoconnectHelper.set_rc_out(File.open( File.join($app_path, "rhoconnect.log" ), "w"), File.open( File.join($app_path, "rhoconnect_err.log" ), "w"))
 	RhoconnectHelper.set_redis_out File.open( File.join($app_path, "redis.log" ), "w") if $rhoconnect_use_redis
+	rhodes_log = File.join($spec_path, 'rhoconnect_push_client', 'rholog.txt')
+	File.unlink(rhodes_log) if File.exists?(rhodes_log)
 	RhoconnectHelper.set_enable_redis($rhoconnect_use_redis)
 	RhoconnectHelper.set_enable_resque(false)
 
@@ -102,10 +103,11 @@ def run_apps(platform)
 			end
 
 			FileUtils.chdir File.join($spec_path, 'rhoconnect_push_client')
-			# system("rake clean:android") # FIXME:
-			puts "\nBuilding rhodes app ..."
-			puts "rake device:#{$platform}:debug"
-			system("rake device:#{$platform}:debug").should == true
+			if build_required
+				puts "\nBuilding rhodes app ..."
+				puts "rake device:#{$platform}:debug"
+				system("rake device:#{$platform}:debug").should == true
+			end
 
 			puts "Install rhoconnect push service ..."
 			push_service_apk = File.join($rhoelements_root,'libs','rhoconnect-push-service','rhoconnect-push-service.apk')
@@ -126,6 +128,7 @@ def run_apps(platform)
 				:out => File.open(File.join($spec_path, 'rhoconnect_push_client', 'rholog.txt'), "w"))
 			puts "Starting logcat process with pid: #{$logcat_pid}"
 		else
+			# Using emulator
 			puts "Configure android emulator ..."
 			Rake::Task["config:android:emulator"].invoke
 			AndroidTools.run_emulator( :wipe => true )
@@ -140,9 +143,9 @@ def run_apps(platform)
 			puts "Install rhoconnect push service"
 			push_service_apk = File.join($rhoelements_root,'libs','rhoconnect-push-service','rhoconnect-push-service.apk')
 			AndroidTools.load_app_and_run("-e", push_service_apk, "")
+
 			puts 'Building and starting rhodes application ...'
 			FileUtils.chdir File.join($spec_path, 'rhoconnect_push_client')
-			system("rake clean:android") # FIXME:
 			system("rake run:#{$platform}").should == true
 		end
 	else
@@ -153,7 +156,6 @@ def run_apps(platform)
 		puts
 		#
 	end
-
 	puts "Running push specs ..."
 	puts
 rescue Exception => e
