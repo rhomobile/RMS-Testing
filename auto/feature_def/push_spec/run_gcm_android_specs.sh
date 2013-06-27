47746268f5906344709476a47c42ebe48d36520f
Alexander Babichev <alexb@rhomobile.com>
2013-06-27 12:40:14 -0700

    Update GCM push specs

--- auto/feature_def/push_spec/run_gcm_android_specs.sh
+++ auto/feature_def/push_spec/run_gcm_android_specs.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Google Cloud Messaging specs for android device
 
+# path to local rhodes workspace
+pushd ../../../../rhodes
+git pull origin master
+popd
+
 pushd gcm_push_client
 rake clean:android
 popd
