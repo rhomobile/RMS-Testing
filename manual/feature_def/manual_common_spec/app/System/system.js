7c9d8a0a81f1f43696a02f854aeefe13686c3016
Vladimir Musulainen <vmusulainen@gmail.com>
2013-06-28 18:03:30 +0400

    modify manual system tests

--- manual/feature_def/manual_common_spec/app/System/system.js
+++ manual/feature_def/manual_common_spec/app/System/system.js
@@ -1,17 +1,7 @@
-function setapplicationIconBadge(iconbadgeValue)
-{
-	Rho.System.applicationIconBadge=iconbadgeValue;
-}
-
 function getapplicationIconBadge()
 {
 	data = Rho.System.getProperty('applicationIconBadge');
 	$("#Rho_System_getapplicationIconBadge span.result").text(JSON.stringify(data));
-}
-
-function sethttpProxyURI()
-{
-	Rho.System.httpProxyURI='http://wwwgate0.mot.com:1080';
 }
 
 function gethttpProxyURI()
@@ -26,21 +16,11 @@ function getisEmulator()
 	$("#Rho_System_isEmulator span.result").text(JSON.stringify(data));
 }
 
-function setlockWindowSize(lockwindowsizeValue)
-{
-	Rho.System.lockWindowSize=lockwindowsizeValue;
-}
-
 function getlockWindowSize()
 {
 	//data = Rho.System.getProperty('lockWindowSize');
 	data = Rho.System.lockWindowSize;
 	$("#Rho_System_lockWindowSize span.result").text(JSON.stringify(data));
-}
-
-function setscreenAutoRotate(autorotateValue)
-{
-	Rho.System.screenAutoRotate=autorotateValue;
 }
 
 function getscreenAutoRotate()
@@ -55,20 +35,10 @@ function getscreenOrientation()
 	$("#Rho_System_screenOrientation span.result").text(JSON.stringify(data));
 }
 
-function setscreenSleeping(screensleepingValue)
-{
-	Rho.System.screenSleeping=screensleepingValue;
-}
-
 function getscreenSleeping()
 {
 	data = Rho.System.getProperty('screenSleeping');
 	$("#Rho_System_screenSleeping span.result").text(JSON.stringify(data));
-}
-
-function setkeyboardState(keyboardstatValue)
-{
-	Rho.System.keyboardState=keyboardstatValue;
 }
 
 function getkeyboardState()
@@ -78,34 +48,12 @@ function getkeyboardState()
 }
 
 
-function callbringToFront()
-{
-	Rho.System.bringToFront();
-}
-
-function calldeleteFolder(deletefolderValue)
-{
-	Rho.System.deleteFolder(deletefolderValue);
-}
-
-function callgetStartParams()
-{
-	var getStartParamsValue = new Array();
-	getStartParamsValue = Rho.System.getStartParams();
-	alert(getStartParamsValue);
-}
-
 function callisApplicationInstalled(aString) {
     var isInstalled = Rho.System.isApplicationInstalled(aString)
     if (isInstalled)
         alert(aString + " application is installed in the device");
     else
         alert(aString + " application is not installed in the device");
-}
-
-function callopenUrl(openurlValue)
-{
-	Rho.System.openUrl(openurlValue);
 }
 
 function callreplaceCurrentBundleWindows()
@@ -131,16 +79,6 @@ function callreplaceCurrentBundlewp8()
 function callreplaceCurrentBundlece()
 {
 	Rho.System.replaceCurrentBundle("httpServerUrl+'/download_app?device=wp8'", {do_not_restart_app:false, not_thread_mode:true});
-}
-
-function CallsetDoNotBackupAttribute(pathValue, doNotBackupValue)
-{
-	Rho.System.setDoNotBackupAttribute(pathValue, doNotBackupValue);
-}
-
-function callsetProperty(propertyName, propertyValue)
-{
-	Rho.System.setProperty(propertyName,propertyValue);
 }
 
 function callgetProperty(propertyName)
@@ -170,7 +108,7 @@ function download_filetolocal(srvHttpDownloadImageUrl, filetoinstall)
                 url: srvHttpDownloadImageUrl,
                 filename: fname
             };
-	
+
     Rho.Network.downloadFile(downloadfileProps);
 	return fname;
 }
