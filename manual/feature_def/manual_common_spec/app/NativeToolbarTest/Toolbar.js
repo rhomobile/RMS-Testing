500e2f9368d5e7ad160fef8cbd3ccadda768063d
deva varaprasad reddy <devavaraprasad.reddy@gmail.com>
2013-07-08 16:17:31 +0530

    added code for mask color

--- manual/feature_def/manual_common_spec/app/NativeToolbarTest/Toolbar.js
+++ manual/feature_def/manual_common_spec/app/NativeToolbarTest/Toolbar.js
@@ -9,10 +9,20 @@ function toolCreate() {
         {label: 'Options', action: 'options'},
         {label: 'Exit', action: 'exit'}
     ];
-    var toolProperties = {maskColor: '', viewHeight: 100};
+    var toolProperties = {backgroundColor: 0x00804F,maskColor: 0xFF0000, viewHeight: 100};
     Rho.NativeToolbar.create(toolElements, toolProperties);
 }
 
+function toolMask() {
+    var toolElements = new Array();
+    toolElements = [
+        {label: 'back', action: 'back'},
+        {label: 'Home', action: 'Home'},
+        {action: '/app/NativeToolbarTest/Page1.html', label: "[BUTTON]",icon: '/app/NativeToolbarTest/redi.png'}
+    ];
+    var toolProperties = {backgroundColor: 0x00804F,maskColor: 0xFF0000, viewHeight: 100};
+    Rho.NativeToolbar.create(toolElements, toolProperties);
+}
 
 function toolIconLabelMask() {
     var toolElements = new Array();
