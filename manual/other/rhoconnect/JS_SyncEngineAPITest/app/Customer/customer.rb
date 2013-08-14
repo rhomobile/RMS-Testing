afb78d777b6dcce56e052393f0b88e74a7d6d239
Bhakta Ranjan Satapathy <bhakta@motorolasolutions.com>
2013-08-14 15:39:16 +0530

    Adding Rhoconnect Applications created by qmpk64- Amit

---
+++ manual/other/rhoconnect/JS_SyncEngineAPITest/app/Customer/customer.rb
@@ -0,0 +1,11 @@
+# The model has already been created by the framework, and extends Rhom::RhomObject
+# You can add more methods here
+class Customer
+  include Rhom::PropertyBag
+
+  # Uncomment the following line to enable sync with Customer.
+  enable :sync
+  #set :sync_priority, 1
+  #add model specifc code here
+
+end
