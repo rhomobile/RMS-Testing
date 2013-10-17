cf2aa4ef317c38a872129ee303d0079871a558ac
Alex Epifanoff <alex.epifanoff@gmail.com>
2013-10-17 19:30:27 +0400

    websocket specs only for android integrate ws server

--- server/ws-server.rb
+++ server/ws-server.rb
@@ -1,23 +1,25 @@
 require 'em-websocket'
 
-EM.run {
-    EM::WebSocket.run(:host => '192.168.0.102', :port => 8084) do |ws|
-        ws.onopen { |handshake|
-            puts "WebSocket connection open"
-            
-            # Access properties on the EM::WebSocket::Handshake object, e.g.
-            # path, query_string, origin, headers
-            
-            # Publish message to the client
-#            ws.send "Hello Client, you connected to #{handshake.path}"
-        }
+$ws = nil
+
+def startWsServer( host, port )
+    $ws = EM.run {
+        EM::WebSocket.run(:host => '192.168.0.102', :port => 8084) do |ws|
+            ws.onopen { |handshake|
+                puts "WebSocket connection open"
+            }
         
-        ws.onclose { puts "Connection closed" }
+            ws.onclose { puts "Connection closed" }
         
-        ws.onmessage { |msg|
-            puts "Recieved message: #{msg}"
-            ws.send "Pong: #{msg}"
-        }
-    end
-}
+            ws.onmessage { |msg|
+                puts "Recieved message: #{msg}"
+                ws.send "Pong: #{msg}"
+            }
+        end
+    }
+end
+
+def stopWsServer()
+    EM.stop()
+end
 
