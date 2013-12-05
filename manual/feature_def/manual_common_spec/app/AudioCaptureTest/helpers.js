1fa7dc5156672bc73d43545b62c8d23d42be1761
Vladimir Musulainen <vmusulainen@gmail.com>
2013-12-05 16:29:01 +0400

    Modified audiocapture tests

--- manual/feature_def/manual_common_spec/app/AudioCaptureTest/helpers.js
+++ manual/feature_def/manual_common_spec/app/AudioCaptureTest/helpers.js
@@ -1,4 +1,12 @@
-function filename() {
-    return Rho.RhoFile.join(Rho.Application.userFolder, 'captured.mp4')
+function filenameMp4() {
+    return filenameWithExtension("mp4");
+}
+
+function filename3gpp() {
+    return filenameWithExtension("3gpp");
+}
+
+function filenameWithExtension(aString) {
+    return Rho.RhoFile.join(Rho.Application.userFolder, "captured." + aString);
 }
 
