13b43d7b1f8c4973552ffb5214e91a26353dc05a
Alexey Tikhvinsky <alexey@rhomobile.com>
2013-12-12 21:59:43 +0400

    ApplicationMessage test draft for android [#61405076]

--- TestApp/app/System/controller.rb
+++ TestApp/app/System/controller.rb
@@ -5,7 +5,20 @@ class SystemController < Rho::RhoController
 
   def getStartParameters
      startParameters = Rho::System.getStartParams();
-     jsCommand = 'alert(JSON.stringify("' + startParameters + '"));';
+     jsCommand = 'alert("start params: "+JSON.stringify("' + startParameters + '"));';
+    Rho::WebView.executeJavascript(jsCommand)
+  end
+  def getApplicationMessage
+     startParameters = Rho::System.getApplicationMessage();
+     jsCommand = 'alert("app message: "+JSON.stringify("' + startParameters + '"));';
+    Rho::WebView.executeJavascript(jsCommand)
+  end
+  def getStartParametersHandler(startParameters)
+    jsCommand = 'alert("start params: "+JSON.stringify("' + startParameters + '"));';
+    Rho::WebView.executeJavascript(jsCommand)
+  end
+  def getApplicationMessageHandler(startParameters)
+    jsCommand = 'alert("app message: "+JSON.stringify("' + startParameters + '"));';
     Rho::WebView.executeJavascript(jsCommand)
   end
 end
