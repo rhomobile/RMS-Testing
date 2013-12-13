1158df129d20687dcab748854541c0efd2d7f76c
anton vishnevski <antov@rhomobile.com>
2013-12-13 05:13:15 +0400

    intents, wm implementation

--- TestApp/app/System/controller.rb
+++ TestApp/app/System/controller.rb
@@ -5,20 +5,20 @@ class SystemController < Rho::RhoController
 
   def getStartParameters
      startParameters = Rho::System.getStartParams();
-     jsCommand = 'alert("start params: "+JSON.stringify("' + startParameters + '"));';
+     jsCommand = 'alert("start params: "+JSON.stringify("' + startParameters.to_s + '"));';
     Rho::WebView.executeJavascript(jsCommand)
   end
   def getApplicationMessage
      startParameters = Rho::System.getApplicationMessage();
-     jsCommand = 'alert("app message: "+JSON.stringify("' + startParameters + '"));';
+     jsCommand = 'alert("app message: "+JSON.stringify("' + startParameters.to_s + '"));';
     Rho::WebView.executeJavascript(jsCommand)
   end
   def getStartParametersHandler(startParameters)
-    jsCommand = 'alert("start params: "+JSON.stringify("' + startParameters + '"));';
+    jsCommand = 'alert("start params: "+JSON.stringify("' + startParameters.to_s + '"));';
     Rho::WebView.executeJavascript(jsCommand)
   end
   def getApplicationMessageHandler(startParameters)
-    jsCommand = 'alert("app message: "+JSON.stringify("' + startParameters + '"));';
+    jsCommand = 'alert("app message: "+JSON.stringify("' + startParameters.to_s + '"));';
     Rho::WebView.executeJavascript(jsCommand)
   end
 end
