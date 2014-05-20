78b475255787346164f08e7c0d8be835d5bbb763
Alex Epifanoff <alex.epifanoff@gmail.com>
2014-05-20 18:11:09 +0400

    support http streaming

--- server/server.rb
+++ server/server.rb
@@ -5,7 +5,102 @@ require 'openssl'
 require 'net/http'
 require 'rexml/document'
 
-#WEBrick::Config::General[:DoNotReverseLookup] = true
+
+
+
+
+
+
+
+
+
+module WEBrick
+
+  class HTTPRequest
+    # Generate HTTP/1.1 100 continue response. See
+    # http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-core/18459
+    def continue
+      if self['expect'] == '100-continue' && @config[:HTTPVersion] >= "1.1"
+        @socket.write "HTTP/#{@config[:HTTPVersion]} 100 continue\r\n\r\n"
+        @header.delete('expect')
+      end
+    end
+  end
+
+  class HTTPResponse
+    alias :orig_setup_header :setup_header
+    # Correct termination of streamed HTTP/1.1 responses. See
+    # http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-core/18454 and
+    # http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-core/18565
+    def setup_header
+      orig_setup_header
+      unless chunked? || @header['content-length']
+        @header['connection'] = "close"
+        @keep_alive = false
+      end
+    end
+
+    # Allow streaming of zipfile entry. See
+    # http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-core/18460
+    def send_body(socket)
+      if @body.respond_to?(:read) then send_body_io(socket)
+      elsif @body.respond_to?(:call) then send_body_proc(socket)
+      else send_body_string(socket)
+      end
+    end
+
+    # If the response body is a proc, then we invoke it and pass in
+    # an object which supports "write" and "<<" methods. This allows
+    # arbitary output streaming.
+    def send_body_proc(socket)
+      if @request_method == "HEAD"
+        # do nothing
+      elsif chunked?
+        @body.call(ChunkedWrapper.new(socket, self))
+        _write_data(socket, "0#{CRLF}#{CRLF}")
+      else
+        size = @header['content-length'].to_i
+        @body.call(socket)   # TODO: StreamWrapper which supports offset, size
+        @sent_size = size
+      end
+    end
+
+    class ChunkedWrapper
+      def initialize(socket, resp)
+        @socket = socket
+        @resp = resp
+      end
+      def write(buf)
+        return if buf.empty?
+        data = ""
+        data << format("%x", buf.size) << CRLF
+        data << buf << CRLF
+        socket = @socket
+        @resp.instance_eval {
+          _write_data(socket, data)
+          @sent_size += buf.size
+        }
+      end
+      alias :<< :write
+    end
+  end
+end
+
+if RUBY_VERSION < "1.9"
+  old_verbose, $VERBOSE = $VERBOSE, nil
+  # Increase from default of 4K for efficiency, similar to
+  # http://svn.ruby-lang.org/cgi-bin/viewvc.cgi/branches/ruby_1_8/lib/net/protocol.rb?r1=11708&r2=12092
+  # In trunk the default is 64K and can be adjusted using :InputBufferSize,
+  # :OutputBufferSize
+  WEBrick::HTTPRequest::BUFSIZE = 16384
+  WEBrick::HTTPResponse::BUFSIZE = 16384
+  $VERBOSE = old_verbose
+end
+
+
+
+
+
 
 #puts "argv : #{ARGV}"
 def localip
@@ -249,27 +344,18 @@ end
 
 $local_server.mount_proc( '/time_stream' ) do |req, res|
   res.content_type = 'text/event-stream'
-  r,w = IO.pipe
-  res.body = r
   res.chunked = true
-  t = Thread.new do
-    begin
+  
+  res.body = proc { |w|
       20.times do
-        w << 'data: ' << Time.now.to_s << "\x0D\x0A"
+        w << 'data: ' + Time.now.to_s + "\x0D\x0A"
         w << "\x0D\x0A"
-        w.flush
         sleep 1
       end
-      w << 'event: end' << "\x0D\x0A"
-      w << 'data: end'  << "\x0D\x0A"
+      w << 'event: end' + "\x0D\x0A"
+      w << 'data: end'  + "\x0D\x0A"
       w << "\x0D\x0A"
-      w.flush
-    rescue => err
-      puts "#{err.inspect}"
-    ensure
-      w.close()
-    end
-  end
+  }
 end
 
 #Secure server mount points
