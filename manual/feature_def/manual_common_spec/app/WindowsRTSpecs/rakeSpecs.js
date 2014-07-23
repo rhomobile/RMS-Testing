550402b44ada745424fde40bac155249be27b4bb
Arun Kumar P <arunkp10@gmail.com>
2014-07-23 17:33:23 +0530

    Modified spec.md and added rakeSpec.js

--- manual/feature_def/manual_common_spec/app/WindowsRTSpecs/rakeSpecs.js
+++ manual/feature_def/manual_common_spec/app/WindowsRTSpecs/rakeSpecs.js
@@ -4,7 +4,7 @@ describe("WindowsRT spec", function () {
         var spec = new ManualSpec(jasmine, window.document);
         spec.addGoal(jasmine.getEnv().currentSpec.description);
         spec.addPrecondition("run \"rake build:wp8:rhobundle_noext\" command in CLI");
-        spec.addExpectation("File *** must appears in directory bin/");
+        spec.addExpectation("File *** must appears in directory bin/. Passed if the application installed and launched successfully in the WindowsRT device.");
         spec.displayScenario();
         runs(function () {
             //spec.addResult("", "");
@@ -17,7 +17,7 @@ describe("WindowsRT spec", function () {
         var spec = new ManualSpec(jasmine, window.document);
         spec.addGoal(jasmine.getEnv().currentSpec.description);
         spec.addPrecondition("run \"rake clean:wp8 \" command in CLI");
-        spec.addExpectation("And what should happen?");
+        spec.addExpectation("Contents in the bin/ directory should get cleaned/flushed.");
         spec.displayScenario();
         runs(function () {
             //spec.addResult("", "");
@@ -30,7 +30,7 @@ describe("WindowsRT spec", function () {
         var spec = new ManualSpec(jasmine, window.document);
         spec.addGoal(jasmine.getEnv().currentSpec.description);
         spec.addPrecondition("run \"rake device:wp8:production\" command in CLI");
-        spec.addExpectation("And what should happen?");
+        spec.addExpectation("File .xap should be placed under 'app/bin/target/wp8'. Passed if the application installed and launched successfully in the WindowsRT device.");
         spec.displayScenario();
         runs(function () {
             //spec.addResult("", "");
@@ -39,11 +39,11 @@ describe("WindowsRT spec", function () {
         });
     });
 
-    it("should build production for device", function () {
+    it("should build production for emulator", function () {
         var spec = new ManualSpec(jasmine, window.document);
         spec.addGoal(jasmine.getEnv().currentSpec.description);
         spec.addPrecondition("run \"rake emulator:wp8:production\" command in CLI");
-        spec.addExpectation("And what should happen?");
+        spec.addExpectation("File .xap should be placed under 'app/bin/target/wp8'. Passed if the application installed and launched successfully in the WindowsRT device.");
         spec.displayScenario();
         runs(function () {
             //spec.addResult("", "");
@@ -56,7 +56,7 @@ describe("WindowsRT spec", function () {
         var spec = new ManualSpec(jasmine, window.document);
         spec.addGoal(jasmine.getEnv().currentSpec.description);
         spec.addPrecondition("run \"rake run:wp8\" command in CLI");
-        spec.addExpectation("And what should happen?");
+        spec.addExpectation("Application should get build and installed with the emulator.");
         spec.displayScenario();
         runs(function () {
             //spec.addResult("", "");
@@ -69,7 +69,7 @@ describe("WindowsRT spec", function () {
         var spec = new ManualSpec(jasmine, window.document);
         spec.addGoal(jasmine.getEnv().currentSpec.description);
         spec.addPrecondition("run \"rake run:wp8:device\" command in CLI");
-        spec.addExpectation("And what should happen?");
+        spec.addExpectation("Application should get build and installed with the device.");
         spec.displayScenario();
         runs(function () {
             //spec.addResult("", "");
@@ -83,7 +83,7 @@ describe("WindowsRT spec", function () {
         var spec = new ManualSpec(jasmine, window.document);
         spec.addGoal(jasmine.getEnv().currentSpec.description);
         spec.addPrecondition("run \"rake run:wp8:rhosimulator\" command in CLI");
-        spec.addExpectation("And what should happen?");
+        spec.addExpectation("Application should get installed and launched with rhosimulator.");
         spec.displayScenario();
         runs(function () {
             //spec.addResult("", "");
@@ -92,5 +92,51 @@ describe("WindowsRT spec", function () {
         });
     });
 
+    it("should run all auto test cases with auto_common_spec", function () {
+        var spec = new ManualSpec(jasmine, window.document);
+        spec.addGoal(jasmine.getEnv().currentSpec.description);
+        spec.addPrecondition("auto_common_spec application build and installed with WindowsRT device.");
+        spec.addPrecondition('Run auto test for the objects given below:');
+        spec.addStep("Application");
+        spec.addStep("Database");
+        spec.addStep("Log");
+        spec.addStep("NativeTabBar");
+        spec.addStep("NativeToolBar");
+        spec.addStep("ORM and ORMModel");
+        spec.addStep("Rhoconnect-client");
+        spec.addStep("RhoFile");
+        spec.addStep("System");
+        spec.addStep("WebView");
+        spec.addExpectation("All auto test cases related to the object supported by wp8 should pass.");
+        spec.displayScenario();
+        runs(function () {
+            //spec.addResult("", "");
+            spec.displayResults();
+            spec.waitForResponse();
+        });
+    });
 
+    it("should run all manual test cases with manual_common_spec", function () {
+        var spec = new ManualSpec(jasmine, window.document);
+        spec.addGoal(jasmine.getEnv().currentSpec.description);
+        spec.addPrecondition("manual_common_spec application build and installed with WindowsRT device.");
+        spec.addPrecondition("Run manual test for the objects given below.");
+        spec.addStep("Application");
+        spec.addStep("Database");
+        spec.addStep("Log");
+        spec.addStep("NativeTabBar");
+        spec.addStep("NativeToolBar");
+        spec.addStep("ORM and ORMModel");
+        spec.addStep("Rhoconnect-client");
+        spec.addStep("RhoFile");
+        spec.addStep("System");
+        spec.addStep("WebView");
+        spec.addExpectation("All manual test cases related to the object supported by wp8 should pass");
+        spec.displayScenario();
+        runs(function () {
+            //spec.addResult("", "");
+            spec.displayResults();
+            spec.waitForResponse();
+        });
+    });
 });
--- manual/feature_def/manual_common_spec/app/WindowsRTSpecs/spec.md
+++ manual/feature_def/manual_common_spec/app/WindowsRTSpecs/spec.md
@@ -1,74 +1,35 @@
 
 #Windows RT Support#
-##Requirement description##
-*Rho application support in WinRT devices, Build for WP8 should work in both WP8 and WinRT devices.*  
-####API supported with WinRT####
-- Application
-- Database
-- Log
-- NativeTabBar
-- NativeToolBar
-- Network
-- ORM and ORMModel
-- Rhoconnect Client
-- RhoFile
-- System
-- WebView  
-- 2.2 Non Depricated API support
+This document describes RhoElements support of Windows RT as a target platform.  
 
-##Test Cases##
-    
-####TestCase ID : VTxxx-01  
-**Goal          : [win8] Support to build WinRT application via Command Line**  
-**Steps         :**  
-1 Open your build path in command line / terminal.  
-2 Enter command `rake build:wp8:production`  
-**Expected      : An installable file should be created under `app/bin/target/wp8/` File  must appears in directory bin**  
-  
-####TestCase ID : VTxxx-02  
-**Goal          : [win8] Support to deploy WinRT application with WinRT device via Command Line**  
-**Steps         :**  
-1 Open your build path in command line / terminal.  
-2 Enter command `rake run:wp8:device`  
-**Expected      : The built applciation should get installed in the device directly.** 
-  
-####TestCase ID : VTxxx-03  
-**Goal          : [win8] Support to winRT clean via command line**  
-**Steps         :**  
-1 Open your build path in command line / terminal.  
-2 Enter command `rake clean:wp8`  
-**Expected      : The project should get clean and the `app/bin` folder should get cleaned/flushed.** 
-  
-####TestCase ID : VTxxx-04  
-**Goal          : [win8] Support to run winRT application on RhoSimulator**  
-**Steps         :**  
-1 Open your build path in command line / terminal.  
-2 Enter command `rake run:wp8:rhosimulator`  
-**Expected      : The project should get clean and the `app/bin` folder should get cleaned/flushed.** 
-  
-####TestCase ID : VTxxx-05  
-**Goal          : [win8] Support to build winRT application using RhoMobile Studio**  
-**Steps         :**  
-1 Launch RhoMobile Studio.  
-2 Create new RhoMobile application project using RhoMobile Studio.  
-3 Select `Menu->RhoMobile->Production build`.   
-4 Select the `Project-> Actual application`, `Platform-> WindowsRT`, and `Build-> Local`.  
-**Expected      : An installable file should be created under `app/bin/target/wp8/` File  must appears in directory bin.** 
-  
-####TestCase ID : VTxxx-06  
-**Goal          : [win8] Support to clean winRT application using RhoMobile Studio**  
-**Steps         :**  
-1 Launch RhoMobile Studio.  
-2 Create new RhoMobile application project using RhoMobile Studio.  
-3 Select `Menu->Project->Clean...`.   
-4 Select the project to get cleaned and select `ok`.  
-**Expected      : The project should get clean and the `app/bin` folder should get cleaned/flushed.**  
+##Overview:##
+Windows RT is a new generation of Windows operating system. It has a narrower API set than modern Windows 8.x for desktops and partially even Windows Phone 8.x, so some features of RhoElements need to be reimplemented using new “RT”-style APIs.
 
-####TestCase ID : VTxxx-07  
-**Goal          : [win8] Application build time should not be more than any other platform**  
-**Steps         :**  
-1 Launch RhoMobile Studio.  
-2 Create new RhoMobile application project using RhoMobile Studio.  
-3 Select `Menu->RhoMobile->Production build`.   
-4 Select the `Project-> Actual application`, `Platform-> WindowsRT`, and `Build-> Local`.  
-**Expected      : The application build time should not be longer than any other platform.**  
+###Requirement description:###
+Rho application support in WinRT devices, Build for WP8 should work in both WP8 and WinRT devices.  
+
+####Rake commands support:####
+- `rake build:wp8:rhobundle_noext` : *Support to build windowsRT rhobundle*
+- `rake clean:wp8` : *Support to clean windowsRT project*
+- `rake device:wp8:production` : *Support to build a .xap file to install on the windowsRT device*
+- `rake emulator:wp8:production` : *Support to build a .xap file to install on the windowsRT device*
+- `rake run:wp8` : *Support to build and install .xap file with emulator*
+- `rake run:wp8:device` : *Support to build and install .xap file with windowsRT device*
+- `rake run:wp8:rhosimulator` : *Support to run the application with rhosimulator*
+
+####API support:####
+- Application : `Rho.Application`
+- Database : `Rho.Database`
+- Log : `Rho.Log`
+- NativeTabBar : `Rho.NativeTabbar`
+- NativeToolBar : `Rho.NativeToolbar`
+- Network : `Rho.Network`
+- ORM and ORMModel : `Rho.ORM`
+- RhoConnectClient : `Rho.RhoConnectClient`
+- RhoFile : `Rho.RhoFile`
+- System : `Rho.System`
+- WebView  : `Rho.WebView`
+
+####Application support:####
+Initially only pure JavaScript applications will be supported on Windows RT.  
+Later on we may support Ruby applications as well
