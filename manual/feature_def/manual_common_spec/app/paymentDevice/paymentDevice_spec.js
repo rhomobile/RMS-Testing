1de9a28df597a687454f020e0005a0ac0b169422
Arun Kumar P <arunkp10@gmail.com>
2014-07-31 18:02:43 +0530

    Added initial version of test spec for paymentDevice.

---
+++ manual/feature_def/manual_common_spec/app/paymentDevice/paymentDevice_spec.js
@@ -0,0 +1,53 @@
+if(isAndroidPlatform() && Rho.System.deviceName=="Motorola Solutions MC40"){
+	describe("Test spec for D180 payment device support", function(){
+		//Should support for the enumarate to get the object of the paymentDevice
+		it("Should support \"enumarate()\" method to get the object of the paymentDevice", function(){
+			//test implementation TBD: To Be Done
+		});
+
+		//Should support method Open()
+		it("Should support for the method \"Open()\" with the instance of paymentDevice object, to open the connection with paymentDevice.", function(){
+			//test implementation TBD : To Be Done.
+		});
+
+		//Should support property isOpened
+		it("Should support \"isOpened\" property with the instance of paymentDevice object, to check the connection already exists or not.", function(){
+			//test implementation TBD : To Be Done.
+		});
+
+		//Should support for the method close()
+		it("Should support \"close()\" method with the instance of paymentDevice object, to close the connection with paymentDevice.", function(){
+			//test implementation TBD : To Be Done.
+		});
+
+		//Should support method enableKeypad()
+		it("Should support \"enableKeypad()\" method with the instance of paymentDevice Object, to enable/display keypad to enter pin or etc., ", function(){
+			//test implementation TBD : To Be Done.
+		});
+
+		//Should support method disableKeypad()
+		it("Should support \"disableKeypad()\" method with the instance of paymentDevice object, to disable/remove keypad from the view.", function(){
+			//test implementation TBD : To Be Done.
+		});
+
+		//Should support method readCardData()
+		it("Should support \"readCardData()\" method with the instance of the paymentDevice object to read card data", function(){
+			//test implementation TBD : To Be Done.
+		});
+
+		//Should support method promptPin()
+		it("Should support \"promptPin()\" method with the instance of the paymentDevice object, to prompt for PIN.", function(){
+			//test implementation TBD : To Be Done.
+		});
+
+		//Should support method promptMenu()
+		it("Should support \"promptMenu()\" method with the instance of the paymentDevice object, to prompt menu items", function(){
+			//test implementation TBD : To Be Done.
+		});
+
+		//Should support method promptAdditionalInfo()
+		it("Should support \"promptAdditionalInfo()\" method with the instace of the paymentDevice object, to prompt/show additional information", function(){
+			//test implementation TBD : To Be Done.
+		});
+	});
+}
