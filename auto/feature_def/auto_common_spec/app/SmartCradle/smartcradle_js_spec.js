130f47614c286f4599e65ec52d7e42cdab358ae5
darryncampbell <darryncampbell@motorolasolutions.com>
2014-09-21 14:04:19 +0100

    Tests for Get / Set Propert(ies)

--- auto/feature_def/auto_common_spec/app/SmartCradle/smartcradle_js_spec.js
+++ auto/feature_def/auto_common_spec/app/SmartCradle/smartcradle_js_spec.js
@@ -5,8 +5,8 @@
 			enableflag = false;
 		});
 				
-
-		describe("Read / Write Properties of the Smart Cradle", function() {
+		
+		describe("Read / Write Properties of the Smart Cradle (via Accessors)", function() {
 
 			beforeEach(function() 
 			{
@@ -17,9 +17,6 @@
 				Rho.SmartCradle.rowId = 1;
 				var readValue = Rho.SmartCradle.rowId;
 				expect(readValue).toBe(1);
-				
-				//  I NEED TO DO THIS
-				//  ST might want to add alternative ways of validating this property here, e.g. setProperty, setProperties etc.
 			});
 			
 			it("is able to correctly set the rowId to a valid value (5) ", function() 
@@ -201,8 +198,200 @@
 			
 		});
 		
-				
-		describe("Read Only Properties of the Smart Cradle", function() {
+		describe("Read / Write Properties of the Smart Cradle (via Get / Set Property)", function() {
+
+			beforeEach(function() 
+			{
+			});
+
+			it("is able to correctly set the rowId to a valid value (1) ", function() 
+			{
+				Rho.SmartCradle.setProperty("rowID", "1");
+				var readValue = Rho.SmartCradle.getProperty("rowID");
+				expect(readValue).toBe("1");
+			});
+			
+			it("is able to correctly set the rowId to a valid value (5) ", function() 
+			{
+				Rho.SmartCradle.setProperty("rowID", "5");
+				var readValue = Rho.SmartCradle.getProperty("rowID");
+				expect(readValue).toBe("5");
+			});
+
+			it("is able to accept an unrealistically large rowId (10000) ", function() 
+			{
+				Rho.SmartCradle.setProperty("RowID", "10000");
+				var readValue = Rho.SmartCradle.getProperty("rowID");
+				expect(readValue).toBe("10000");
+			});
+
+			it("is able to fail gracefully when setting the rowId to an invalid value (-1) ", function() 
+			{
+				Rho.SmartCradle.setProperty("rowID", "1");
+				//  Expect this value to fail gracefully
+				Rho.SmartCradle.setProperty("rowID", "-1");
+				var readValue = Rho.SmartCradle.getProperty("RowID");
+				expect(readValue).toBe("1");
+			});
+
+			it("is able to fail gracefully when setting the rowId to an invalid value (-100) ", function() 
+			{
+				Rho.SmartCradle.setProperty("rowID", "1");
+				//  Expect this value to fail gracefully
+				Rho.SmartCradle.setProperty("rowID", "-100");
+				var readValue = Rho.SmartCradle.getProperty("RowID");
+				expect(readValue).toBe("1");
+			});
+
+			it("is able to correctly set the columnId to a valid value (1) ", function() 
+			{
+				Rho.SmartCradle.setProperty("columnID", "1");
+				var readValue = Rho.SmartCradle.getProperty("columnID");
+				expect(readValue).toBe("1");
+			});
+			
+			it("is able to correctly set the columnID to a valid value (5) ", function() 
+			{
+				Rho.SmartCradle.setProperty("columnID", "5");
+				var readValue = Rho.SmartCradle.getProperty("columnID");
+				expect(readValue).toBe("5");
+			});
+
+			it("is able to accept an unrealistically large columnID (10000) ", function() 
+			{
+				Rho.SmartCradle.setProperty("columnID", "10000");
+				var readValue = Rho.SmartCradle.getProperty("columnID");
+				expect(readValue).toBe("10000");
+			});
+
+			it("is able to fail gracefully when setting the columnID to an invalid value (-1) ", function() 
+			{
+				Rho.SmartCradle.setProperty("columnID", "1");
+				//  Expect this value to fail gracefully
+				Rho.SmartCradle.setProperty("columnID", "-1");
+				var readValue = Rho.SmartCradle.getProperty("columnID");
+				expect(readValue).toBe("1");
+			});
+
+			it("is able to fail gracefully when setting the columnID to an invalid value (-100) ", function() 
+			{
+				Rho.SmartCradle.setProperty("columnID", "1");
+				//  Expect this value to fail gracefully
+				Rho.SmartCradle.setProperty("columnID", "-100");
+				var readValue = Rho.SmartCradle.getProperty("columnID");
+				expect(readValue).toBe("1");
+			});
+
+			it("is able to correctly set the wallID to a valid value (1) ", function() 
+			{
+				Rho.SmartCradle.setProperty("wallID", "1");
+				var readValue = Rho.SmartCradle.getProperty("wallID");
+				expect(readValue).toBe("1");
+			});
+			
+			it("is able to correctly set the wallID to a valid value (5) ", function() 
+			{
+				Rho.SmartCradle.setProperty("wallID", "5");
+				var readValue = Rho.SmartCradle.getProperty("wallID");
+				expect(readValue).toBe("5");
+			});
+
+			it("is able to accept an unrealistically large wallID (10000) ", function() 
+			{
+				Rho.SmartCradle.setProperty("wallID", "10000");
+				var readValue = Rho.SmartCradle.getProperty("wallID");
+				expect(readValue).toBe("10000");
+			});
+
+			it("is able to fail gracefully when setting the wallID to an invalid value (-1) ", function() 
+			{
+				Rho.SmartCradle.setProperty("wallID", "1");
+				//  Expect this value to fail gracefully
+				Rho.SmartCradle.setProperty("wallID", "-1");
+				var readValue = Rho.SmartCradle.getProperty("wallID");
+				expect(readValue).toBe("1");
+			});
+
+			it("is able to fail gracefully when setting the wallID to an invalid value (-100) ", function() 
+			{
+				Rho.SmartCradle.setProperty("wallID", "1");
+				//  Expect this value to fail gracefully
+				Rho.SmartCradle.setProperty("wallID", "-100");
+				var readValue = Rho.SmartCradle.getProperty("wallID");
+				expect(readValue).toBe("1");
+			});
+
+			it("is able to correctly set the chargeState to a valid value ('fast') ", function() 
+			{
+				Rho.SmartCradle.setProperty("chargeState", "fast");
+				var readValue = Rho.SmartCradle.getProperty("chargeState");
+				expect(readValue).toBe("fast");
+
+				//  Now test this with different cases
+				Rho.SmartCradle.setProperty("chargeState", "FAST");
+				var readValue = Rho.SmartCradle.getProperty("chargeState");
+				expect(readValue).toBe("fast");
+
+				Rho.SmartCradle.setProperty("chargeState", "fASt");
+				var readValue = Rho.SmartCradle.getProperty("chargeState");
+				expect(readValue).toBe("fast");
+
+				Rho.SmartCradle.setProperty("chargeState", "FasT");
+				var readValue = Rho.SmartCradle.getProperty("ChargeState");
+				expect(readValue).toBe("fast");
+			});
+			
+			it("is able to correctly set the chargeState to a valid value ('slow') ", function() 
+			{
+				Rho.SmartCradle.setProperty("chargeState", "slow");
+				var readValue = Rho.SmartCradle.getProperty("chargeState");
+				expect(readValue).toBe("slow");
+
+				//  Now test this with different cases
+				Rho.SmartCradle.setProperty("chargeState", "SLOW");
+				var readValue = Rho.SmartCradle.getProperty("chargeState");
+				expect(readValue).toBe("slow");
+
+				Rho.SmartCradle.setProperty("chargeState", "sLOw");
+				var readValue = Rho.SmartCradle.getProperty("chargeState");
+				expect(readValue).toBe("slow");
+
+				Rho.SmartCradle.setProperty("chargeState", "SloW");
+				var readValue = Rho.SmartCradle.getProperty("ChargeState");
+				expect(readValue).toBe("slow");
+			});
+
+			it("is able to correctly set the chargeState to an invalid value ('bunnies') ", function() 
+			{
+				Rho.SmartCradle.setProperty("chargeState", "slow");
+				//  Expect this to fail gracefully
+				Rho.SmartCradle.setProperty("chargeState", "bunnies");
+				var readValue = Rho.SmartCradle.getProperty("chargeState");
+				expect(readValue).toBe("slow");
+			});
+
+			it("is able to correctly set the chargeState to an invalid value ('fastslow') ", function() 
+			{
+				Rho.SmartCradle.setProperty("chargeState", "slow");
+				//  Expect this to fail gracefully
+				Rho.SmartCradle.setProperty("chargeState", "fastslow");
+				var readValue = Rho.SmartCradle.getProperty("chargeState");
+				expect(readValue).toBe("slow");
+			});
+
+			it("is able to correctly set the chargeState to an invalid value ('slowfast') ", function() 
+			{
+				Rho.SmartCradle.setProperty("chargeState", "slow");
+				//  Expect this to fail gracefully
+				Rho.SmartCradle.setProperty("chargeState", "slowfast");
+				var readValue = Rho.SmartCradle.getProperty("chargeState");
+				expect(readValue).toBe("slow");
+			});
+			
+		});			
+
+		
+		describe("Read Only Properties of the Smart Cradle (Via Accessors)", function() {
 
 			beforeEach(function() 
 			{
@@ -221,14 +410,14 @@
 			{
 				var readValue = Rho.SmartCradle.partNumber;
 				console.log("Read property Part Number: " + readValue);
-				expect(true).toBe(false);;
+				expect(true).toBe(false);
 			});
 
 			it("is able to successfully retrieve the serialNumber", function() 
 			{
 				var readValue = Rho.SmartCradle.serialNumber;
 				console.log("Read property Serial Number: " + readValue);
-				expect(true).toBe(false);;
+				expect(true).toBe(false);
 			});
 
 			it("is able to successfully retrieve the mfgDate", function() 
@@ -236,7 +425,7 @@
 				//  mfgDate should be in the correct format
 				var readValue = Rho.SmartCradle.mfgDate;
 				console.log("Read property MFG Date (DD-MM-YY): " + readValue);
-				expect(true).toBe(false);;
+				expect(true).toBe(false);
 			});
 
 			it("is able to successfully retrieve the driverVersion", function() 
@@ -270,7 +459,138 @@
 			});
 
 		});
+
+
+		describe("Read Only Properties of the Smart Cradle (Via Get / Set Property)", function() {
+
+			beforeEach(function() 
+			{
+
+			});
+					
+			it("is able to successfully retrieve the hardwareId", function() 
+			{
+				//  ALSO TEST WE CAN'T SET IT!!!
+				var readValue = Rho.SmartCradle.getProperty("hardwareId");
+				console.log("Read property Hardware ID: " + readValue);
+				expect(true).toBe(false);;
+			});
+
+			it("is able to successfully retrieve the partNumber", function() 
+			{
+				var readValue = Rho.SmartCradle.getProperty("partNumber");
+				console.log("Read property Part Number: " + readValue);
+				expect(true).toBe(false);;
+			});
+
+			it("is able to successfully retrieve the serialNumber", function() 
+			{
+				var readValue = Rho.SmartCradle.getProperty("serialNumber");
+				console.log("Read property Serial Number: " + readValue);
+				expect(true).toBe(false);;
+			});
+
+			it("is able to successfully retrieve the mfgDate", function() 
+			{
+				//  mfgDate should be in the correct format
+				var readValue = Rho.SmartCradle.getProperty("mfgDate");
+				console.log("Read property MFG Date (DD-MM-YY): " + readValue);
+				expect(true).toBe(false);;
+			});
+
+			it("is able to successfully retrieve the driverVersion", function() 
+			{
+				var readValue = Rho.SmartCradle.getProperty("driverVersion");
+				console.log("Read property Driver Version: " + readValue);
+				//  Expect the result to be in the form m.n
+				var regex = /[0-9]+\.[0-9]+/
+				var matched = regex.exec(readValue);
+				expect(matched).not.toBe(null);
+			});
+
+			it("is able to successfully retrieve the cApiVersion", function() 
+			{
+				var readValue = Rho.SmartCradle.getProperty("cApiVersion");
+				console.log("Read property C API: " + readValue);
+				//  Expect the result to be in the form m.n
+				var regex = /[0-9]+\.[0-9]+/
+				var matched = regex.exec(readValue);
+				expect(matched).not.toBe(null);
+			});
+
+			it("is able to successfully retrieve the firmwareVersion", function() 
+			{
+				var readValue = Rho.SmartCradle.getProperty("firmwareVersion");
+				console.log("Read property Firmware Version: " + readValue);
+				//  Expect the result to be in the form m.n
+				var regex = /[0-9]+\.[0-9]+/
+				var matched = regex.exec(readValue);
+				expect(matched).not.toBe(null);
+			});
+
+		});
+			
+
+		describe("Able to Set and Retrieve properties via the Set / Get Properties methods", function() {
+
+			beforeEach(function() 
+			{
+
+			});
+
+			it("is able to set all writable properties using the SetProperties method & retrieve using GetProperties", function() 
+			{
+				var propsToSet = {"chargeState":"fast","rowId":1,"columnId":1,"wallId":1}
+				var propsToGet = ["chargeState","rowId","columnId","wallId"]
+				Rho.SmartCradle.setProperties(propsToSet);
+				var retrievedProps = Rho.SmartCradle.getProperties(propsToGet);
+				console.log(JSON.stringify(retrievedProps));
 				
+				expect(retrievedProps.rowId).toBe("1");
+				expect(retrievedProps.columnId).toBe("1");
+				expect(retrievedProps.wallId).toBe("1");
+				expect(retrievedProps.chargeState).toBe("fast");
+				
+				var propsToSet = {"chargeState":"slow","rowId":5,"columnId":4,"wallId":3}
+				var propsToGet = ["chargeState","rowId","columnId","wallId"]
+				Rho.SmartCradle.setProperties(propsToSet);
+				var retrievedProps = Rho.SmartCradle.getProperties(propsToGet);
+				
+				expect(retrievedProps.rowId).toBe("5");
+				expect(retrievedProps.columnId).toBe("4");
+				expect(retrievedProps.wallId).toBe("3");
+				expect(retrievedProps.chargeState).toBe("slow");				
+			});
+
+			it("is able to retrieve all properties using the GetAllProperties method", function() 
+			{
+				var propsToSet = {"chargeState":"fast","rowId":1,"columnId":1,"wallId":1}
+				Rho.SmartCradle.setProperties(propsToSet);
+				var retrievedProps = Rho.SmartCradle.getAllProperties();
+				console.log(JSON.stringify(retrievedProps));
+				
+				expect(retrievedProps.rowId).toBe("1");
+				expect(retrievedProps.columnId).toBe("1");
+				expect(retrievedProps.wallId).toBe("1");
+				expect(retrievedProps.chargeState).toBe("fast");
+				//  TODO
+				expect(retrievedProps.hardwareId).toBe("TBD");
+				expect(retrievedProps.partNumber).toBe("TBD");
+				expect(retrievedProps.serialNumber).toBe("TBD");
+				expect(retrievedProps.mfgDate).toBe("TBD");
+				var driverVersionRegex = /[0-9]+\.[0-9]+/
+				var driverVersionMatched = regex.exec(retrievedProps.driverVersion);
+				expect(driverVersionMatched).not.toBe(null);
+				var cApiVersionRegex = /[0-9]+\.[0-9]+/
+				var cApiVersionMatched = regex.exec(retrievedProps.cApiVersion);
+				expect(cApiVersionMatched).not.toBe(null);
+				var firmwareVersionRegex = /[0-9]+\.[0-9]+/
+				var firmwareVersionMatched = regex.exec(retrievedProps.firmwareVersion);
+				expect(firmwareVersionMatched).not.toBe(null);			
+			});
+
+		});
+			
 		//  Note - Unlock can only be tested in a manual way
 				
 	});
