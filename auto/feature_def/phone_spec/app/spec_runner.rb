335175de73b54ef31459954928cc67a5ac16f8b0
Mikhail Malinin <malinin.work@gmail.com>
2014-10-15 18:53:01 +0400

    auto/phone_spec: update ruby spec selection, also update build yml sdkversion of rhodes and android

--- auto/feature_def/phone_spec/app/spec_runner.rb
+++ auto/feature_def/phone_spec/app/spec_runner.rb
@@ -1,5 +1,15 @@
 require 'mspec'
 
+RUN_RUBY_CONTAINERS_SPEC = true #false
+
+RUN_RUBY_EXTENSION_SPEC = true #false
+
+RUN_COREAPI_SPEC = true #false
+
+RUN_ORM_SPEC = true #false
+
+RUN_EXTENDED_API_SPEC = true #false
+
 class SpecRunner < MSpecScript
 
   def initialize
@@ -9,69 +19,81 @@ class SpecRunner < MSpecScript
     # turn on exception backtrace
     MSpec.backtrace = true
 
-    config[:files] << "spec/string/end_with_spec"
-    config[:files] << "spec/string/start_with_spec"
-    config[:files] << "spec/string/replace_spec"
-    config[:files] << "spec/string/split_spec"
-    config[:files] << "spec/string/strip_spec"
-    config[:files] << "spec/string/rstrip_spec"
-    config[:files] << "spec/string/lstrip_spec"
-    config[:files] << "spec/string/slice_spec"
-    config[:files] << "spec/array/pack_spec"
-
-    config[:files] << "spec/log_spec"
-
-    config[:files] << "spec/rho_spec"
-
-    config[:files] << "spec/crypt_spec"
-
-    config[:files] << "spec/json_spec"
-    config[:files] << "spec/xml_spec"
-    config[:files] << "spec/rhofile_spec"
-    config[:files] << "spec/file_commonapi_spec"
-
-    config[:files] << "spec/network_spec"
-    config[:files] << "spec/asynchttp_spec"
-
-    config[:files] << "spec/date_spec"
-    config[:files] << "spec/bsearch_spec"
-
-    config[:files] << "spec/rho_controller_spec"
-
-    config[:files] << "spec/serialPort_spec"
-
-    config[:files] << [ "spec/rhom_object_spec",
-                        [ {:schema_model=>true, :sync_model=>true},  {:schema_model=>true, :sync_model=>false},
-                          {:schema_model=>false, :sync_model=>true} , {:schema_model=>false, :sync_model=>false} ] ]
-
-    # should be enabled as extension
-    # config[:files] << "spec/javascriptvm_spec" if System.get_property('platform') == 'APPLE'
-
-    if System.get_property('platform') != 'WP8'
-      config[:files] << "spec/contacts_spec" unless System.get_property('platform') == 'WINDOWS_DESKTOP'
-
-      # Disable events specs on Android because emulator doesn't contain Calendar provider
-      config[:files] << "spec/events_spec"  unless System.get_property('platform') == 'WINDOWS_DESKTOP' or (System.get_property('platform') == 'ANDROID' and Rho::System.isEmulator )
-
-      config[:files] << "spec/barcode_spec" unless System.get_property('platform') == 'WINDOWS' || System.get_property('platform') == 'WINDOWS_DESKTOP'
-      config[:files] << "spec/mapview_spec"  unless System.get_property('platform') == 'WINDOWS' || System.get_property('platform') == 'WINDOWS_DESKTOP'
+    if RUN_RUBY_CONTAINERS_SPEC
+        config[:files] << "spec/string/end_with_spec"
+        config[:files] << "spec/string/start_with_spec"
+        config[:files] << "spec/string/replace_spec"
+        config[:files] << "spec/string/split_spec"
+        config[:files] << "spec/string/strip_spec"
+        config[:files] << "spec/string/rstrip_spec"
+        config[:files] << "spec/string/lstrip_spec"
+        config[:files] << "spec/string/slice_spec"
+        config[:files] << "spec/array/pack_spec"
     end
 
-    config[:files] << "spec/bundle_update_spec" if !Rho::System.isRhoSimulator && (System.get_property('platform') == 'APPLE' || System.get_property('platform') == 'ANDROID' || System.get_property('platform') == 'WINDOWS' || System.get_property('platform') == 'WINDOWS_DESKTOP')
+    if RUN_RUBY_EXTENSION_SPEC
+        config[:files] << "spec/json_spec"
+        config[:files] << "spec/xml_spec"
+        config[:files] << "spec/date_spec"
+        config[:files] << "spec/bsearch_spec"
+        config[:files] << "spec/crypt_spec"
+    end
 
-    config[:files] << "spec/nativebar_spec" if System.get_property('platform') != 'Blackberry' && System.get_property('platform') != 'WP8'
-    config[:files] << "spec/navbar_spec" if System.get_property('platform') == 'APPLE' || System.get_property('platform') == 'ANDROID'
+    if RUN_COREAPI_SPEC
+        config[:files] << "spec/log_spec"
 
-    config[:files] << "spec/xruby_spec" if defined? RHO_ME
+        config[:files] << "spec/rho_spec"
 
-    # if !(System.get_property('platform') == 'Blackberry' && (System::get_property('os_version') =~ /^6\.0/)) && System.get_property('platform') != 'WP8'
-    #    config[:files] << "spec/uri_spec"
-    # end
-    #if !defined?( RHO_ME )
+        config[:files] << "spec/rhofile_spec"
+        config[:files] << "spec/file_commonapi_spec"
+
+        config[:files] << "spec/network_spec"
+        config[:files] << "spec/asynchttp_spec"
+
+        config[:files] << "spec/rho_controller_spec"
+
+        config[:files] << "spec/serialPort_spec"
+    end
+
+    if RUN_ORM_SPEC
+
+        modelz = []
+
+         modelz << {:schema_model=>true, :sync_model=>true}
+         modelz << {:schema_model=>true, :sync_model=>false}
+         modelz << {:schema_model=>false, :sync_model=>true}
+         modelz << {:schema_model=>false, :sync_model=>false}
+
+        config[:files] << [ "spec/rhom_object_spec", modelz ]
+    end
+
+    if RUN_EXTENDED_API_SPEC
+        if System.get_property('platform') != 'WP8'
+          config[:files] << "spec/contacts_spec" unless System.get_property('platform') == 'WINDOWS_DESKTOP'
+
+          # Disable events specs on Android because emulator doesn't contain Calendar provider
+          config[:files] << "spec/events_spec"  unless System.get_property('platform') == 'WINDOWS_DESKTOP' or (System.get_property('platform') == 'ANDROID' and Rho::System.isEmulator )
+
+          config[:files] << "spec/barcode_spec" unless System.get_property('platform') == 'WINDOWS' || System.get_property('platform') == 'WINDOWS_DESKTOP'
+          config[:files] << "spec/mapview_spec"  unless System.get_property('platform') == 'WINDOWS' || System.get_property('platform') == 'WINDOWS_DESKTOP'
+        end
+
+        config[:files] << "spec/bundle_update_spec" if !Rho::System.isRhoSimulator && (System.get_property('platform') == 'APPLE' || System.get_property('platform') == 'ANDROID' || System.get_property('platform') == 'WINDOWS' || System.get_property('platform') == 'WINDOWS_DESKTOP')
+
+        config[:files] << "spec/nativebar_spec" if System.get_property('platform') != 'Blackberry' && System.get_property('platform') != 'WP8'
+        config[:files] << "spec/navbar_spec" if System.get_property('platform') == 'APPLE' || System.get_property('platform') == 'ANDROID'
+
+        config[:files] << "spec/xruby_spec" if defined? RHO_ME
+
+        # if !(System.get_property('platform') == 'Blackberry' && (System::get_property('os_version') =~ /^6\.0/)) && System.get_property('platform') != 'WP8'
+        #    config[:files] << "spec/uri_spec"
+        # end
+        #if !defined?( RHO_ME )
 =begin
-    config[:files] << "spec/database_spec" #unless System.get_property('platform') == 'WINDOWS' && System.get_property('is_emulator')
+        config[:files] << "spec/database_spec" #unless System.get_property('platform') == 'WINDOWS' && System.get_property('is_emulator')
 =end
-    #end
+        #end
+    end
   end
 
   def run
