41d2ce3ad394a84d34f634e216aa5cb096440492
Abhineet Agarwal <abhineet.agarwal@zebra.com>
2014-11-25 14:35:18 +0530

    Update symbolDevice_spec.js
    calibration update

--- manual/feature_def/manual_common_spec/app/symbolDevice/symbolDevice_spec.js
+++ manual/feature_def/manual_common_spec/app/symbolDevice/symbolDevice_spec.js
@@ -1,64 +1,25 @@
 describe('Symbol Device test cases : ', function(){
 	describe('Non Power test cases : ', function(){
 		if(isAnyWindowsFamilyPlatform()){
-			it("Should caliberate successfully with out optional callback", function(){
+			it("Should calibrate successfully with out optional callback", function(){
 				var spec = new ManualSpec(jasmine, window.document);
-		    	spec.addGoal("Should support for the method \"caliberate()\", to trigger the caliberate screen.");
+		    	spec.addGoal("Should support for the method \"calibrate()\", to trigger the calibrate screen.");
 		        spec.addStep("Press 'RunTest' button");
 		        spec.addExpectation('Test passed, only if calibration screen is brought foreground by pressing Run Test button.');
 		        spec.displayScenario();
 		        spec.waitForButtonPressing("Run test");
 		        runs(function(){
-		        	Rho.Device.caliberate();
+		        	Rho.Device.calibrate();
 		        });
 		        runs(function(){
 	                spec.waitForResponse();
 				});
 			});
-			it("Should caliberate successfully with optional callback and press 'esc' ", function(){
+			it("Should not trigger calibrate callback when caliberation done via settings", function(){
 				var spec = new ManualSpec(jasmine, window.document);
-		    	spec.addGoal("Should support for the method \"caliberate()\", to trigger the caliberate screen.");
-		        spec.addStep("Press 'RunTest' button.");
-		        spec.addStep("Press 'esc' button from the device keypad.");
-		        spec.addExpectation('Test passed, only if calibration screen is brought foreground and by pressing esc button caliberation should be unsuccessful.');
-		        spec.displayScenario();
-		        spec.waitForButtonPressing("Run test");
-		        runs(function(){
-		        	var caliberateCB = function(data){
-		        		spec.addResult("Status : ", data.status);
-		        		spec.addResult("Message : ", data.message);
-		        	}
-		        	Rho.Device.caliberate();
-		        });
-		        runs(function(){
-	                spec.waitForResponse();
-				});
-			});
-			it("Should caliberate successfully with optional callback and press 'Ok' ", function(){
-				var spec = new ManualSpec(jasmine, window.document);
-		    	spec.addGoal("Should support for the method \"caliberate()\", to trigger the caliberate screen.");
-		        spec.addStep("Press 'RunTest' button.");
-		        spec.addStep("Do caliberation on the device.");
-		        spec.addStep("Press 'Ok' button from the device keypad.");
-		        spec.addExpectation('Test passed, only if calibration screen is brought foreground and by pressing Ok button after performing caliberation should trigger with success callback.');
-		        spec.displayScenario();
-		        spec.waitForButtonPressing("Run test");
-		        runs(function(){
-		        	var caliberateCB = function(data){
-		        		spec.addResult("Status : ", data.status);
-		        		spec.addResult("Message : ", data.message);
-		        	}
-		        	Rho.Device.caliberate();
-		        });
-		        runs(function(){
-	                spec.waitForResponse();
-				});
-			});
-			it("Should not trigger caliberate callback when caliberation done via settings", function(){
-				var spec = new ManualSpec(jasmine, window.document);
-		    	spec.addGoal("Should not trigger caliberate callback when caliberation done via settings");
+		    	spec.addGoal("Should not trigger calibrate callback when caliberation done via settings");
 		        spec.addStep("Launch the test application press run test button and put it in the background.");
-		        spec.addStep("Navigate to settings -> stylus -> Caliberate.");
+		        spec.addStep("Navigate to settings -> stylus -> calibrate.");
 		        spec.addStep("Perform caliberation and press Ok button once done with it.");
 		        spec.addExpectation('Test passed only, if doing calibration via settings did not trigger callback.');
 		        spec.displayScenario();
@@ -194,23 +155,6 @@ describe('Symbol Device test cases : ', function(){
 					Rho.Device.idle(idleCB);
 					setTimeout(function(){
 						Rho.Device.wake(wakeCB);
-					},10000);
-				});
-				runs(function(){
-					spec.waitForResponse();
-				});
-			});
-			it("Should change device mode from suspend to wake by executing wake method.", function(){
-				var spec = new ManualSpec(jasmine, window.document);
-				spec.addGoal("Should change device mode from suspend to wake by executing wake method.");
-				spec.addStep("Press Run Test buton.");
-				spec.addExpectation("Observe that device goes suspend mode and after 10 sec bring back to wake mode.");
-				spec.displayScenario();
-				spec.waitForButtonPressing("Run test");
-				runs(function(){
-					Rho.Device.suspend();
-					setTimeout(function(){
-						Rho.Device.wake();
 					},10000);
 				});
 				runs(function(){
