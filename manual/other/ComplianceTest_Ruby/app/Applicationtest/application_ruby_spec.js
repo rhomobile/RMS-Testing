14d64882b3516601339dbdcac7ba2ef8af734900
ashikd <ashikdelamp@gmail.com>
2015-03-31 15:43:26 +0530

    adding application ruby compliance tests

---
+++ manual/other/ComplianceTest_Ruby/app/Applicationtest/application_ruby_spec.js
@@ -0,0 +1,69 @@
+describe("Application Module Test Starts Here", function() {
+
+    beforeEach(function() {
+        data ='';
+        document.getElementById('verificationResult').innerHTML = "";
+    });
+
+
+    it("VT200-0613 | get BadLink Uri without setting it", function() {
+        dispTestCaseRunning("VT200-0613 | get BadLink Uri without setting it");
+        dispExpectedResult("Application should return the default badlink uri " );
+        
+        _result.waitToRunTest();
+
+        runs(function(){
+            Ruby.call('Applicationtest','badlink_uri');
+        });
+
+        _result.waitForResponse();
+
+    });
+
+    it("VT200-0614 | Security Token Not passed", function() {
+
+        dispTestCaseRunning("VT200-0614 | Security Token Not passed");
+        dispExpectedResult("it should return  false as the security token check  was passed " );
+
+        _result.waitToRunTest();
+        
+        runs(function(){
+            Ruby.call('Applicationtest','securitytoken');
+        });
+        
+        _result.waitForResponse();
+
+    });
+
+    it("VT200-0615 | set StartUri to a web page", function() {
+
+        dispTestCaseRunning("VT200-0615 | set StartUri to a web page");
+        dispExpectedResult("application should navigate to the start uri provided after opening " );
+
+        _result.waitToRunTest();
+        
+        runs(function(){
+            Ruby.call('Applicationtest','start_uri');
+        });
+        
+        _result.waitForResponse();
+
+    });
+
+    it("VT200-0616 | Database File path with Local as Partition", function() {
+
+
+        dispTestCaseRunning("VT200-0616 | Database File path with Local as Partition");
+        dispExpectedResult("should return the location of the database file with partition name local" );
+        
+        _result.waitToRunTest();
+        
+        runs(function(){
+            Ruby.call('Applicationtest','database_file');
+        });
+
+        _result.waitForResponse();
+
+    });
+
+});
---
+++ manual/other/ComplianceTest_Ruby/app/Applicationtest/controller.rb
@@ -0,0 +1,117 @@
+require 'rho/rhocontroller'
+require 'json'
+require 'helpers/application_helper'
+require 'helpers/browser_helper'
+
+class ApplicationtestController < Rho::RhoController
+
+
+def minimize
+  Rho::Application.minimize()
+end
+  
+def restore
+  Rho::Application.restore()
+end
+  
+def quit
+  Rho::Application.quit()
+end
+
+def start_original
+  Rho::Application.startURI= '/app/index.html'
+end
+
+def start_local
+  Rho::Application.startURI= '/app/loading.html'
+end
+  
+def start_web
+  Rho::Application.startURI= 'http://www.google.com'
+end
+   
+def start_null
+  Rho::Application.startURI= ''
+end
+   
+def start_invalid
+  Rho::Application.startURI= '/app/idontExist.html'
+end
+
+def set_title
+  Rho::Application.title ='MyTitle'
+end
+
+# callback method
+def menu_callback
+  Rho::Log.info("menu_callback", "LOG_TEST")
+end
+
+def menu_test
+  Rho::Application.nativeMenu = [
+    {'label'=>'Home', 'action'=>'Home'},
+    {'label'=>'separator', 'action'=>'separator'},
+    {'label'=>'Options', 'action'=>'options'},
+    {'label'=>'Log', 'action'=>'log'},
+    {'label'=>'Exit', 'action'=>'exit'},
+    {'label'=>'Refresh', 'action'=>'refresh'}, 
+    {'label'=>'Callback', 'action'=> url_for(:action => :menu_callback)}
+  ]
+end
+
+def menu_default
+  Rho::Application.nativeMenu = Rho::Application.defaultNativeMenu
+end
+
+def menu_reservedItem
+  Rho::Application.nativeMenu = [
+    {'label'=>'Options', 'action'=> 'Options'},
+    {'label'=>'Exit', 'action'=> 'exit'},
+    {'label'=>'Log', 'action'=> 'Log'},
+    {'label'=>'Refresh', 'action'=> 'Refresh'},
+    { 'label'=>'Home', 'action'=> 'Home'},
+    {'label'=>'Load a page', 'action'=> '/app/loading.html'}
+  ]
+end
+
+def menu_jscall
+  Rho::Application.nativeMenu = [
+    {'label'=>'Log', 'action'=> 'Log'},
+    {'label'=>'fullscreen', 'action'=> 'fullscreen'},
+    {'label'=>'Exit', 'action'=>'javascript=>onQuit()'},
+    {'label'=>'separator', 'action'=> 'separator '},
+    {'label'=>'Refresh', 'action'=> 'Refresh'},
+    { 'label'=>'Home', 'action'=> 'Home'},
+    {'label'=>'Load a page', 'action'=> '/app/loading.html'}
+  ]
+end
+
+def badlink_uri
+  @data1 = Rho::Application.badLinkURI
+  if @data1
+    @data = @data.to_s
+  else
+    @data = ''
+  end
+  Rho::WebView.executeJavascript('document.getElementById("verificationResult").innerHTML= "'+@data+'";')
+end
+
+def securitytoken
+  @data1 = Rho::Application.securityTokenNotPassed
+  @data = @data1.to_s
+  Rho::WebView.executeJavascript('document.getElementById("verificationResult").innerHTML= "'+@data+'";')
+end
+
+def start_uri
+  @data = Rho::Application.startURI
+  Rho::WebView.executeJavascript('document.getElementById("verificationResult").innerHTML= "'+@data+'";')
+end
+
+def database_file
+  local = Rho::Application.databaseFilePath('local')
+  filepath = Rho::Application.relativeDatabaseBlobFilePath(local)
+  data = Rho::Application.expandDatabaseBlobFilePath(filepath)
+  Rho::WebView.executeJavascript('document.getElementById("verificationResult").innerHTML= "'+data+'";')
+end
+
+end
