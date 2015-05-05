b0b2506179f858399cde04fa1734c457ec4a09b6
ashikd <ashikdelamp@gmail.com>
2015-05-05 12:00:02 +0530

    updating to support wp8 also in camera test app

--- manual/feature_def/manual_spec_5_1/app/Cameratest/camera_spec_camobj1.js
+++ manual/feature_def/manual_spec_5_1/app/Cameratest/camera_spec_camobj1.js
@@ -2,9 +2,9 @@ describe("Camera API Manual Tests", function(){
 	var enumData = Rho.Camera.enumerate();
 	var cam;
 	if(enumData.length == 2){
-		cam = enumData[0].getProperty('cameraType');
+		cam = enumData[0].cameraType;
 	}else{
-		cam = enumData[0].getProperty('cameraType');
+		cam = enumData[0].cameraType;
 		if(cam == 'front' || cam == 'imager'){
 			cam = '';
 		}
