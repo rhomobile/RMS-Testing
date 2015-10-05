fe2ab6d4caa8dc7745b84722d8e5a6b9bd84018a
Raghavendra Kamble <raghavendrakamble@Raghavendras-MacBook-Pro-2.local>
2015-10-05 18:42:26 +0530

    removing project folder

--- auto/other/NewORM/new_orm_spec_js/project/iphone/build/Newormspecjs.build/Release-iphoneos/rhorunner.build/Objects-normal/arm64/app_build_configs.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/raghavendrakamble/ARUN/GIT/RMS-Testing/auto/feature_def/new_orm_spec_js/project/iphone/Rhodes/app_build_configs.c \
-  /Users/raghavendrakamble/.rvm/gems/ruby-1.9.3-p551/gems/rhodes-5.3.0.beta.10/platform/shared/common/RhoConf.h \
-  /Users/raghavendrakamble/.rvm/gems/ruby-1.9.3-p551/gems/rhodes-5.3.0.beta.10/platform/shared/common/RhoPort.h \
-  /Users/raghavendrakamble/.rvm/gems/ruby-1.9.3-p551/gems/rhodes-5.3.0.beta.10/platform/shared/common/RhoDefs.h \
-  /Users/raghavendrakamble/.rvm/gems/ruby-1.9.3-p551/gems/rhodes-5.3.0.beta.10/platform/shared/common/app_build_capabilities.h \
-  /Users/raghavendrakamble/.rvm/gems/ruby-1.9.3-p551/gems/rhodes-5.3.0.beta.10/platform/shared/common/app_build_configs.h \
-  /Users/raghavendrakamble/.rvm/gems/ruby-1.9.3-p551/gems/rhodes-5.3.0.beta.10/platform/shared/common/stat.h
--- auto/other/NewORM/new_orm_spec_js/project/iphone/build/Newormspecjs.build/Release-iphoneos/rhorunner.build/Objects-normal/armv7/extensions.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/raghavendrakamble/ARUN/GIT/RMS-Testing/auto/feature_def/new_orm_spec_js/project/iphone/Rhodes/extensions.c
