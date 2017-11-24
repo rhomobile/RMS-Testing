d1da4e40aa38cbce36a58df36a88dd2cff3e67b2
Dmitry Soldatenkov <dsoldatenkov@tau-technologies.com>
2017-11-24 07:48:48 +0300

    fix output to UI in auto spec run

--- auto/feature_def/framework_spec/app/SpecRunner/controller.rb
+++ auto/feature_def/framework_spec/app/SpecRunner/controller.rb
@@ -19,6 +19,8 @@ class SpecRunnerController < Rho::RhoController
     @exc_count = MSpec.exc_count
     @not_supported = MSpec.not_supported_count
     @count = MSpec.count
+    locations = MSpec.exc_locations
+
 
     total = @count.to_s
     passed = (@count - @exc_count).to_s
@@ -30,7 +32,35 @@ class SpecRunnerController < Rho::RhoController
     puts "***Failed: " + failed
     puts "***Not supported by Rhodes: " + not_supported
 
-    render(string: "{ \"total\":\"#{total}\", \"passed\":\"#{passed}\", \"failed\":\"#{failed}\" }")
+    result = {}
+    result['total'] = total
+    result['passed'] = passed
+    result['failed'] = failed
+    result['locations'] = {}
+    result['not_supported'] = not_supported
+
+
+    @response["headers"]["Content-Type"] = "application/json"
+
+    locations.each do |key,infos|
+      infos_array = []
+
+      infos.each do |info|
+        modified_info = {}
+        info.each do |k,v|
+          if v.is_a?(String)
+            modified_info[k] = v.scrub
+          else
+            modified_info[k] = v
+          end
+        end
+
+        infos_array << modified_info
+      end
+      result['locations'][key] = infos_array
+    end
+
+    render(string: result.to_json)
   end
 
   def run_all_specs
